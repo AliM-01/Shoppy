f80d2e943713f1e4d2bf177540ff9af26134cf28
Ali M <alimousavi-01@outlook.com>
2021-11-11 18:33:54 +0330

    Bug Fixes

--- src/Modules/ShopManagement/Application.Contracts/SM.Application.Contracts/ProductCategory/DTOs/CreateProductCategoryDto.cs
+++ src/Modules/ShopManagement/Application.Contracts/SM.Application.Contracts/ProductCategory/DTOs/CreateProductCategoryDto.cs
@@ -5,34 +5,41 @@ namespace SM.Application.Contracts.ProductCategory.DTOs;
 public class CreateProductCategoryDto
 {
     [Display(Name = "عنوان")]
+    [JsonProperty("title")]
     [Required(ErrorMessage = DomainErrorMessage.RequiredMessage)]
     [MaxLength(100, ErrorMessage = DomainErrorMessage.MaxLengthMessage)]
     public string Title { get; set; }
 
     [Display(Name = "توضیحات")]
+    [JsonProperty("description")]
     [Required(ErrorMessage = DomainErrorMessage.RequiredMessage)]
     [MaxLength(200, ErrorMessage = DomainErrorMessage.MaxLengthMessage)]
     public string Description { get; set; }
 
     [Display(Name = "تصویر")]
+    [JsonProperty("imageFile")]
     public IFormFile ImageFile { get; set; }
 
     [Display(Name = "جزییات تصویر")]
+    [JsonProperty("imageAlt")]
     [Required(ErrorMessage = DomainErrorMessage.RequiredMessage)]
     [MaxLength(100, ErrorMessage = DomainErrorMessage.MaxLengthMessage)]
     public string ImageAlt { get; set; }
 
-    [Display(Name = "عنوان تصویر")]
+    [Display(Name = "عنوان تصویر")]    
+    [JsonProperty("imageTitle")]
     [Required(ErrorMessage = DomainErrorMessage.RequiredMessage)]
     [MaxLength(100, ErrorMessage = DomainErrorMessage.MaxLengthMessage)]
     public string ImageTitle { get; set; }
 
-    [Display(Name = "کلمات کلیدی")]
+    [Display(Name = "کلمات کلیدی")]    
+    [JsonProperty("metaKeywords")]
     [Required(ErrorMessage = DomainErrorMessage.RequiredMessage)]
     [MaxLength(80, ErrorMessage = DomainErrorMessage.MaxLengthMessage)]
     public string MetaKeywords { get; set; }
 
-    [Display(Name = "توضیحات Meta")]
+    [Display(Name = "توضیحات Meta")]    
+    [JsonProperty("metaDescription")]
     [Required(ErrorMessage = DomainErrorMessage.RequiredMessage)]
     [MaxLength(100, ErrorMessage = DomainErrorMessage.MaxLengthMessage)]
     public string MetaDescription { get; set; }
