9279564b5bcf0adb0fdc96a1c488f7f1cd5b4ce5
Ali M <alimousavi-01@outlook.com>
2021-11-11 18:45:37 +0330

    Created Product Domain Model

---
+++ src/Modules/ShopManagement/Domain/SM.Domin/Product/Product.cs
@@ -0,0 +1,51 @@
+﻿namespace SM.Domain.Product;
+public class Product : BaseEntity
+{
+    #region Properties
+
+    [Display(Name = "عنوان")]
+    public string Title { get; set; }
+
+    [Display(Name = "کد")]
+    public string Code { get; set; }
+
+    [Display(Name = "قیمت")]
+    public double UnitPrice { get; set; }
+
+    [Display(Name = "وضعیت موجودی")]
+    public bool IsInStock { get; set; } = true;
+
+    [Display(Name = "توضیح کوتاه")]
+    public string ShortDescription { get; set; }
+
+    [Display(Name = "توضیحات")]
+    public string Description { get; set; }
+
+    [Display(Name = "تصویر")]
+    public string ImagePath { get; set; }
+
+    [Display(Name = "جزییات تصویر")]
+    public string ImageAlt { get; set; }
+
+    [Display(Name = "عنوان تصویر")]
+    public string ImageTitle { get; set; }
+
+    [Display(Name = "کلمات کلیدی")]
+    public string MetaKeywords { get; set; }
+
+    [Display(Name = "توضیحات Meta")]
+    public string MetaDescription { get; set; }
+
+    [Display(Name = "عنوان لینک")]
+    public string Slug { get; set; }
+
+    #endregion
+
+    #region Relations
+
+    public long CategoryId { get; set; }
+
+    public ProductCategory.ProductCategory Category { get; set; }
+
+    #endregion
+}
