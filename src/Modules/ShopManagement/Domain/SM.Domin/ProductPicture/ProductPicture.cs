feb029639cba7d83dcc3233d69b4d9b86d59bf4d
Ali M <alimousavi-01@outlook.com>
2021-11-15 18:15:35 +0330

    Created ProductPicture Domain Entity

---
+++ src/Modules/ShopManagement/Domain/SM.Domin/ProductPicture/ProductPicture.cs
@@ -0,0 +1,23 @@
+﻿namespace SM.Domain.ProductPicture;
+
+public class ProductPicture : BaseEntity
+{
+    #region Properties
+
+    [Display(Name = "تصویر")]
+    public string ImagePath { get; set; }
+
+    [Display(Name = "جزییات تصویر")]
+    public string ImageAlt { get; set; }
+
+    [Display(Name = "عنوان تصویر")]
+    public string ImageTitle { get; set; }
+
+    #endregion
+
+    #region Relations
+
+    public long ProductId { get; set; }
+
+    #endregion
+}
