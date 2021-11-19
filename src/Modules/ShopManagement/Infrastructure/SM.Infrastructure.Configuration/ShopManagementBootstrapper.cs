b523e3889561fa6746ef65b9025b9d4118c7e27f
Ali M <alimousavi-01@outlook.com>
2021-11-19 12:37:10 +0330

    Configured Slider GenericRepository

--- src/Modules/ShopManagement/Infrastructure/SM.Infrastructure.Configuration/ShopManagementBootstrapper.cs
+++ src/Modules/ShopManagement/Infrastructure/SM.Infrastructure.Configuration/ShopManagementBootstrapper.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using SM.Domain.Product;
 using SM.Domain.ProductCategory;
 using SM.Domain.ProductPicture;
+using SM.Domain.Slider;
 using SM.Infrastructure.Persistence.Context;
 
 namespace SM.Infrastructure.Configuration;
@@ -17,6 +18,7 @@ public static class ShopManagementBootstrapper
         services.AddScoped<IGenericRepository<ProductCategory>, GenericRepository<ShopDbContext, ProductCategory>>();
         services.AddScoped<IGenericRepository<Product>, GenericRepository<ShopDbContext, Product>>();
         services.AddScoped<IGenericRepository<ProductPicture>, GenericRepository<ShopDbContext, ProductPicture>>();
+        services.AddScoped<IGenericRepository<Slider>, GenericRepository<ShopDbContext, Slider>>();
 
         services.AddMediatR(typeof(ShopManagementBootstrapper).Assembly);
 
