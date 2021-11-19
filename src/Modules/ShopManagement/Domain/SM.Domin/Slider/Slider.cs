b8f2ce1101e524c3a1dfa937cebc286d58f45c45
Ali M <alimousavi-01@outlook.com>
2021-11-19 12:20:27 +0330

    Created Slider Domain Model

---
+++ src/Modules/ShopManagement/Domain/SM.Domin/Slider/Slider.cs
@@ -0,0 +1,29 @@
+﻿namespace SM.Domain.Slider;
+
+public class Slider : BaseEntity
+{
+    #region Properties
+
+    [Display(Name = "عنوان")]
+    public string Heading { get; set; }
+
+    [Display(Name = "متن")]
+    public string Text { get; set; }
+
+    [Display(Name = "تصویر")]
+    public string ImagePath { get; set; }
+
+    [Display(Name = "جزییات تصویر")]
+    public string ImageAlt { get; set; }
+
+    [Display(Name = "عنوان تصویر")]
+    public string ImageTitle { get; set; }
+
+    [Display(Name = "لینک")]
+    public string BtnLink { get; set; }
+
+    [Display(Name = "متن لینک")]
+    public string BtnText { get; set; }
+
+    #endregion
+}
