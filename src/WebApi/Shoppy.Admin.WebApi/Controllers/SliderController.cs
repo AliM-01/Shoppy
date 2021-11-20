5cb43462bfd273ef351d35186dfa13bd66426362
Ali M <alimousavi-01@outlook.com>
2021-11-20 18:46:08 +0330

    Implemented GetSlidersList Endpoint

--- src/WebApi/Shoppy.Admin.WebApi/Controllers/SliderController.cs
+++ src/WebApi/Shoppy.Admin.WebApi/Controllers/SliderController.cs
@@ -4,6 +4,18 @@ namespace Shoppy.Admin.WebApi.Controllers;
 public class SliderController : BaseApiController
 {
     /// <summary>
+    ///    دریافت لیست اسلایدر ها
+    /// </summary>
+    /// <response code="200">Success</response>
+    [HttpGet(ApiEndpoints.Slider.GetSlidersList)]
+    public async Task<IActionResult> GetSlidersList()
+    {
+        var res = await Mediator.Send(new GetSlidersListQuery());
+
+        return JsonApiResult.Success(res);
+    }
+
+    /// <summary>
     ///    دریافت جزییات اسلایدر
     /// </summary>
     /// <response code="200">Success</response>
