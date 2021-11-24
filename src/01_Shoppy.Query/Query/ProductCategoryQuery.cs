5121681b6f39e50a2c4aba1763fd5dc40dc5050b
Ali M <alimousavi-01@outlook.com>
2021-11-24 12:12:48 +0330

    Implemented ProductCategoryQuery

---
+++ src/01_Shoppy.Query/Query/ProductCategoryQuery.cs
@@ -0,0 +1,30 @@
+﻿using _01_Shoppy.Query.Contracts.ProductCategory;
+using AutoMapper;
+using SM.Infrastructure.Persistence.Context;
+
+namespace _01_Shoppy.Query.Query;
+
+public class ProductCategoryQuery : IProductCategoryQuery
+{
+    #region Ctor
+
+    private readonly ShopDbContext _context;
+    private readonly IMapper _mapper;
+
+    public ProductCategoryQuery(ShopDbContext context, IMapper mapper)
+    {
+        _context = Guard.Against.Null(context, nameof(_context));
+        _mapper = Guard.Against.Null(mapper, nameof(_mapper));
+    }
+
+    #endregion
+
+    public async Task<Response<IEnumerable<ProductCategoryQueryModel>>> GetProductCategories()
+    {
+        var productCategories = await _context.ProductCategories
+            .Select(productCategory => _mapper.Map(productCategory, new ProductCategoryQueryModel()))
+            .ToListAsync();
+
+        return new Response<IEnumerable<ProductCategoryQueryModel>>(productCategories);
+    }
+}
