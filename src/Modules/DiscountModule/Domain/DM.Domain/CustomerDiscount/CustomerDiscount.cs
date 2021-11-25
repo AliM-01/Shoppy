e08c79786e11d2b3e98f9b8000871d7ed8f2820b
Ali M <alimousavi-01@outlook.com>
2021-11-25 22:47:29 +0330

    Created CustomerDiscount Domain Model

---
+++ src/Modules/DiscountModule/Domain/DM.Domain/CustomerDiscount/CustomerDiscount.cs
@@ -0,0 +1,22 @@
+﻿namespace DM.Domain.CustomerDiscount;
+
+public class CustomerDiscount : BaseEntity
+{
+    #region Properties
+
+    public int Rate { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public string Description { get; set; }
+
+    #endregion
+
+    #region Relations
+
+    public long ProductId { get; set; }
+
+    #endregion
+}
