8601c035f174fd9c598a7da860d2a719ecc4f0fd
Ali M <alimousavi-01@outlook.com>
2021-11-28 19:52:24 +0330

    Created CustomerDiscountMapping

---
+++ src/Modules/DiscountModule/Infrastructure/DM.Infrastructure.Persistence/Mappings/CustomerDiscountMapping.cs
@@ -0,0 +1,18 @@
+﻿using DM.Domain.CustomerDiscount;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DM.Infrastructure.Persistence.Mappings;
+public class CustomerDiscountMapping : IEntityTypeConfiguration<CustomerDiscount>
+{
+    public void Configure(EntityTypeBuilder<CustomerDiscount> builder)
+    {
+        builder.ToTable("CustomerDiscounts");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Rate).IsRequired();
+        builder.Property(x => x.Description).HasMaxLength(250).IsRequired();
+        builder.Property(x => x.StartDate).IsRequired();
+        builder.Property(x => x.EndDate).IsRequired();
+    }
+}
