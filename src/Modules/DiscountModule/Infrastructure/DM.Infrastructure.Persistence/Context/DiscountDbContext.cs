2e1ba856ebcac284ad0f48c733865eb5bc98cca6
Ali M <alimousavi-01@outlook.com>
2021-11-28 19:46:02 +0330

    Created DiscountDbContext

---
+++ src/Modules/DiscountModule/Infrastructure/DM.Infrastructure.Persistence/Context/DiscountDbContext.cs
@@ -0,0 +1,26 @@
+﻿using _0_Framework.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace SM.Infrastructure.Persistence.Context;
+public class DiscountDbContext : DbContext
+{
+    #region Ctor
+
+    public DiscountDbContext(DbContextOptions<DiscountDbContext> options) : base(options) { }
+
+    #endregion
+
+    #region On Model Creating
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplySharedDbContextOnModelCreatingConfiguration();
+
+        var assembly = typeof(DiscountDbContext).Assembly;
+        builder.ApplyConfigurationsFromAssembly(assembly);
+
+        base.OnModelCreating(builder);
+    }
+
+    #endregion On Model Creating
+}
