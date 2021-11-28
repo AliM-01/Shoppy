9398f265257e1eb38c36eb0fa1ca175d9e38a714
Ali M <alimousavi-01@outlook.com>
2021-11-28 20:28:01 +0330

    FilterCustomerDiscountsQueryHandler Bug Fixes

--- src/Modules/DiscountModule/Application/DM.Application/CustomerDiscount/QueryHandles/FilterCustomerDiscountsQueryHandler.cs
+++ src/Modules/DiscountModule/Application/DM.Application/CustomerDiscount/QueryHandles/FilterCustomerDiscountsQueryHandler.cs
@@ -2,6 +2,7 @@
 using DM.Application.Contracts.CustomerDiscount.DTOs;
 using DM.Application.Contracts.CustomerDiscount.Queries;
 using Microsoft.EntityFrameworkCore;
+using SM.Domain.Product;
 
 namespace DM.Application.CustomerDiscount.QueryHandles;
 public class FilterCustomerDiscountsQueryHandler : IRequestHandler<FilterCustomerDiscountsQuery, Response<FilterCustomerDiscountDto>>
@@ -9,11 +10,14 @@ public class FilterCustomerDiscountsQueryHandler : IRequestHandler<FilterCustome
     #region Ctor
 
     private readonly IGenericRepository<Domain.CustomerDiscount.CustomerDiscount> _customerDiscountRepository;
+    private readonly IGenericRepository<Product> _productRepository;
     private readonly IMapper _mapper;
 
-    public FilterCustomerDiscountsQueryHandler(IGenericRepository<Domain.CustomerDiscount.CustomerDiscount> customerDiscountRepository, IMapper mapper)
+    public FilterCustomerDiscountsQueryHandler(IGenericRepository<Domain.CustomerDiscount.CustomerDiscount> customerDiscountRepository,
+        IGenericRepository<Product> productRepository, IMapper mapper)
     {
         _customerDiscountRepository = Guard.Against.Null(customerDiscountRepository, nameof(_customerDiscountRepository));
+        _productRepository = Guard.Against.Null(productRepository, nameof(_productRepository));
         _mapper = Guard.Against.Null(mapper, nameof(_mapper));
     }
 
@@ -23,6 +27,12 @@ public class FilterCustomerDiscountsQueryHandler : IRequestHandler<FilterCustome
     {
         var query = _customerDiscountRepository.GetQuery()
             .OrderByDescending(p => p.LastUpdateDate).AsQueryable();
+
+        var products = await _productRepository.GetQuery().Select(x => new
+        {
+            x.Id,
+            x.Title
+        }).ToListAsync();
 
         #region filter
 
@@ -34,8 +44,11 @@ public class FilterCustomerDiscountsQueryHandler : IRequestHandler<FilterCustome
         #region paging
 
         var filteredEntities = await query
-            .Select(product =>
-                _mapper.Map(product, new CustomerDiscountDto()))
+            .Select(discount =>
+                _mapper.Map(discount, new CustomerDiscountDto
+                {
+                    Product = products.FirstOrDefault(x => x.Id == discount.ProductId).Title
+                }))
             .ToListAsync(cancellationToken);
 
         #endregion paging
