0870dd95cd6a2b3ad90c48c6058caf319db8880a
Ali M <alimousavi-01@outlook.com>
2021-11-28 20:19:08 +0330

    Added ToDetailedShamsi Extension Method

--- src/0_Framework/Application/Extensions/DateConvertor.cs
+++ src/0_Framework/Application/Extensions/DateConvertor.cs
@@ -10,5 +10,13 @@ namespace _0_Framework.Application.Extensions
             return pc.GetYear(value) + "/" + pc.GetMonth(value).ToString("00") + "/" +
                    pc.GetDayOfMonth(value).ToString("00");
         }
+
+        public static string ToDetailedShamsi(this DateTime value)
+        {
+            var pc = new PersianCalendar();
+            return pc.GetYear(value) + "/" + pc.GetMonth(value).ToString("00") + "/" +
+                   pc.GetDayOfMonth(value).ToString("00") + " " +
+                   pc.GetHour(value) + " : " + pc.GetMinute(value);
+        }
     }
 }
