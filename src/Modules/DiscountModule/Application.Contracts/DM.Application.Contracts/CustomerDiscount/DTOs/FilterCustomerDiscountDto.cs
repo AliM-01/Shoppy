eeba643df7c1a9383cf380fce08aabeb634a4c0d
Ali M <alimousavi-01@outlook.com>
2021-11-30 13:27:54 +0330

    Bug Fixes

--- src/Modules/DiscountModule/Application.Contracts/DM.Application.Contracts/CustomerDiscount/DTOs/FilterCustomerDiscountDto.cs
+++ src/Modules/DiscountModule/Application.Contracts/DM.Application.Contracts/CustomerDiscount/DTOs/FilterCustomerDiscountDto.cs
@@ -5,7 +5,7 @@ public class FilterCustomerDiscountDto
 {
     [Display(Name = "شناسه محصول")]
     [JsonProperty("productId")]
-    [Range(1, 10000, ErrorMessage = DomainErrorMessage.MaxLengthMessage)]
+    [Range(1, 10000, ErrorMessage = DomainErrorMessage.RequiredMessage)]
     public long ProductId { get; set; }
 
     [JsonProperty("discounts")]
