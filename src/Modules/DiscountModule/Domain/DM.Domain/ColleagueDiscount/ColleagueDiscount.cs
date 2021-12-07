3f25db3f46d0be4bbcaf6308196d219db57ccad2
Ali M <alimousavi-01@outlook.com>
2021-12-07 17:36:33 +0330

    Created ColleagueDiscount Domain Model

---
+++ src/Modules/DiscountModule/Domain/DM.Domain/ColleagueDiscount/ColleagueDiscount.cs
@@ -0,0 +1,18 @@
+﻿namespace DM.Domain.CustomerDiscount;
+
+public class ColleagueDiscount : BaseEntity
+{
+    #region Properties
+
+    public int Rate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    #endregion
+
+    #region Relations
+
+    public long ProductId { get; set; }
+
+    #endregion
+}
