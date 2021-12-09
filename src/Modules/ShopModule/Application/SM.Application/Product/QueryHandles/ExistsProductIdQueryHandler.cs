1f470eb477aca892ca7dd2f3756162fbc9c5cf23
Ali M <alimousavi-01@outlook.com>
2021-12-10 01:26:30 +0330

    Implemented ExistsProductIdQueryHandler

---
+++ src/Modules/ShopModule/Application/SM.Application/Product/QueryHandles/ExistsProductIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SM.Application.Contracts.Product.DTOs;
+using SM.Application.Contracts.Product.Queries;
+
+namespace SM.Application.Product.QueryHandles;
+public class ExistsProductIdQueryHandler : IRequestHandler<ExistsProductIdQuery, Response<ExistsProductIdResponseDto>>
+{
+    #region Ctor
+
+    private readonly IGenericRepository<Domain.Product.Product> _productRepository;
+    private readonly IMapper _mapper;
+
+    public ExistsProductIdQueryHandler(IGenericRepository<Domain.Product.Product> productRepository, IMapper mapper)
+    {
+        _productRepository = Guard.Against.Null(productRepository, nameof(_productRepository));
+        _mapper = Guard.Against.Null(mapper, nameof(_mapper));
+    }
+
+    #endregion
+
+    public async Task<Response<ExistsProductIdResponseDto>> Handle(ExistsProductIdQuery request, CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetQuery()
+            .AsNoTracking().FirstOrDefaultAsync(s => s.Id == request.ProductId);
+
+        if (product is null)
+            return new Response<ExistsProductIdResponseDto>(new ExistsProductIdResponseDto
+            {
+                Exists = false
+            });
+
+        return new Response<ExistsProductIdResponseDto>(new ExistsProductIdResponseDto
+        {
+            Exists = true
+        });
+    }
+}
