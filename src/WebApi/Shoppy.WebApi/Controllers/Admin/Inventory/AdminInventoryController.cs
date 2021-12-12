c466ee1e227687f1176a607f1fe1e8352b784479
Ali M <alimousavi-01@outlook.com>
2021-12-12 20:29:01 +0330

    Implemented FilterInventories Endpoint

--- src/WebApi/Shoppy.WebApi/Controllers/Admin/Inventory/AdminInventoryController.cs
+++ src/WebApi/Shoppy.WebApi/Controllers/Admin/Inventory/AdminInventoryController.cs
@@ -1,5 +1,24 @@
-﻿namespace Shoppy.WebApi.Controllers.Admin.Discount;
+﻿using IM.Application.Contracts.Inventory.DTOs;
+using IM.Application.Contracts.Inventory.Queries;
+
+namespace Shoppy.WebApi.Controllers.Admin.Discount;
 
 public class AdminInventoryController : BaseApiController
 {
+    #region Filter Inventory
+
+    /// <summary>
+    ///    فیلتر انبار
+    /// </summary>
+    /// <response code="200">Success</response>
+    [HttpGet(AdminInventoryApiEndpoints.Inventory.FilterInventories)]
+    public async Task<IActionResult> FilterInventories([FromQuery] FilterInventoryDto filter)
+    {
+        var res = await Mediator.Send(new FilterInventoryQuery(filter));
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
+
 }
--- src/WebApi/Shoppy.WebApi/Endpoints/Admin/AdminInventoryApiEndpoints.cs
+++ src/WebApi/Shoppy.WebApi/Endpoints/Admin/AdminInventoryApiEndpoints.cs
@@ -4,9 +4,11 @@ public static class AdminInventoryApiEndpoints
 {
     private const string BaseAdminInventoryRoute = BaseApiEndpointRoutes.AdminBaseRoute + "/inventory";
 
-    public static class CustomerDiscount
+    public static class Inventory
     {
         private const string BaseInventoryRoute = BaseAdminInventoryRoute;
+
+        public const string FilterInventories = BaseInventoryRoute + "/filter";
 
     }
 
