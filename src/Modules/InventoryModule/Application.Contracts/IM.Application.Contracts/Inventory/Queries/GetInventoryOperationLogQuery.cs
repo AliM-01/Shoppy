eb68295e8222c69535e289344baf437b527eaed2
Ali M <alimousavi-01@outlook.com>
2021-12-21 13:05:00 +0330

    Created GetInventoryOperationLogQuery

---
+++ src/Modules/InventoryModule/Application.Contracts/IM.Application.Contracts/Inventory/Queries/GetInventoryOperationLogQuery.cs
@@ -0,0 +1,14 @@
+﻿using IM.Application.Contracts.Inventory.DTOs;
+using System.Collections.Generic;
+
+namespace IM.Application.Contracts.Inventory.Queries;
+
+public class GetInventoryOperationLogQuery : IRequest<Response<IEnumerable<InventoryOperationDto>>>
+{
+    public GetInventoryOperationLogQuery(long id)
+    {
+        Id = id;
+    }
+
+    public long Id { get; set; }
+}
