fbd8a4f0cd883f25f601b25e0e0619be40f26605
Ali M <alimousavi-01@outlook.com>
2021-12-21 12:54:59 +0330

    Created InventoryOperationDto

---
+++ src/Modules/InventoryModule/Application.Contracts/IM.Application.Contracts/Inventory/DTOs/InventoryOperationDto.cs
@@ -0,0 +1,23 @@
+﻿namespace IM.Application.Contracts.Inventory.DTOs;
+
+public class InventoryOperationDto
+{
+    public long Id { get; set; }
+    public bool OperationType { get; set; }
+
+    public long Count { get; set; }
+
+    public string OperationDate { get; set; }
+
+    public long CurrentCount { get; set; }
+
+    public string Description { get; set; }
+
+    public long InventoryId { get; set; }
+
+    public long OperatorId { get; set; }
+
+    public string Operator { get; set; }
+
+    public long OrderId { get; set; }
+}
