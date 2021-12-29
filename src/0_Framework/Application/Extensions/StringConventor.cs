63bda2234ea7b14a64dde61d8f8fa6808b483fa8
Ali M <alimousavi-01@outlook.com>
2021-12-29 12:38:48 +0330

    Created ToMoney Extension Method

---
+++ src/0_Framework/Application/Extensions/StringConventor.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace _0_Framework.Application.Extensions;
+
+public static class StringConventor
+{
+    public static string ToMoney(this string text)
+    {
+        var number = int.Parse(text);
+        var result = number.ToString("N0", CultureInfo.CreateSpecificCulture("fa-ir"));
+        return result;
+    }
+}
+
