50f6503fbd3d309cf69b7add0122147519017872
Ali M <alimousavi-01@outlook.com>
2022-01-16 21:38:01 +0330

    Created MaxFileSizeAttribute

---
+++ src/0_Framework/Application/Attributes/MaxFileSizeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace _0_Framework.Application.Attributes;
+
+public class MaxFileSizeAttribute : ValidationAttribute
+{
+    private readonly int _maxFileSize;
+
+    public MaxFileSizeAttribute(int maxFileSize)
+    {
+        _maxFileSize = maxFileSize;
+    }
+
+    public override bool IsValid(object value)
+    {
+        var file = value as IFormFile;
+        if (file is null)
+            return true;
+
+        return file.Length <= _maxFileSize;
+    }
+}
+
