bf407670d2ac8482b91abc9c17b1682a6dcc1ca3
Ali M <alimousavi-01@outlook.com>
2022-01-20 08:26:01 +0330

    Created ProductPictureQueryModel

---
+++ src/01_Shoppy.Query/Contracts/ProductPicture/ProductPictureQueryModel.cs
@@ -0,0 +1,12 @@
+﻿namespace _01_Shoppy.Query.Contracts.ProductPicture;
+
+public class ProductPictureQueryModel
+{
+    [Display(Name = "شناسه")]
+    [JsonProperty("id")]
+    public long Id { get; set; }
+
+    [Display(Name = "تصویر")]
+    [JsonProperty("imagePath")]
+    public string ImagePath { get; set; }
+}
