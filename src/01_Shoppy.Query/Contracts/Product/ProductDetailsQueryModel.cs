beb5f4f079acac07813205e8bde40590f0d4e7fc
Ali M <alimousavi-01@outlook.com>
2022-01-20 08:24:43 +0330

    Created ProductDetailsQueryModel

---
+++ src/01_Shoppy.Query/Contracts/Product/ProductDetailsQueryModel.cs
@@ -0,0 +1,28 @@
+﻿namespace _01_Shoppy.Query.Contracts.Product;
+
+public class ProductDetailsQueryModel : ProductQueryModel
+{
+    [Display(Name = "کد")]
+    [JsonProperty("code")]
+    public string Code { get; set; }
+
+    [Display(Name = "توضیح کوتاه")]
+    [JsonProperty("shortDescription")]
+    public string ShortDescription { get; set; }
+
+    [Display(Name = "توضیحات")]
+    [JsonProperty("description")]
+    public string Description { get; set; }
+
+    [Display(Name = "موجودی")]
+    [JsonProperty("inventoryCurrentCount")]
+    public int InventoryCurrentCount { get; set; }
+
+    [Display(Name = "کلمات کلیدی")]
+    [JsonProperty("metaKeywords")]
+    public string MetaKeywords { get; set; }
+
+    [Display(Name = "توضیحات Meta")]
+    [JsonProperty("metaDescription")]
+    public string MetaDescription { get; set; }
+}
