cd108ab8c7dec6396ab214065d15d9117279d608
Ali M <alimousavi-01@outlook.com>
2022-01-20 16:30:51 +0330

    Created ProductFeature Domain Model

---
+++ src/Modules/ShopModule/Domain/SM.Domin/ProductFeature/ProductFeature.cs
@@ -0,0 +1,24 @@
+﻿namespace SM.Domain.ProductFeature;
+
+public class ProductFeature : BaseEntity
+{
+    #region Properties
+
+    [Display(Name = "شناسه محصول")]
+
+    public long? ProductId { get; set; }
+
+    [Display(Name = "عنوان ویژگی")]
+    public string FeatureTitle { get; set; }
+
+    [Display(Name = "مقدار ویژگی")]
+    public string FeatureValue { get; set; }
+
+    #endregion Properties
+
+    #region Relations
+
+    public virtual Product.Product Product { get; set; }
+
+    #endregion Relations
+}
