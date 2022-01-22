bd4d3b6732de51f0a6c0965ba11e79b8975f06e3
Ali M <alimousavi-01@outlook.com>
2022-01-22 21:14:29 +0330

    Created Comment Domain Model

---
+++ src/Modules/CommentModule/Domain/CM.Domain/Comment/Comment.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CM.Domain.Comment;
+
+public class Comment
+{
+    #region Properties
+
+    [Display(Name = "نام")]
+    public string Name { get; set; }
+
+    [Display(Name = "ایمیل")]
+    public string Email { get; set; }
+
+    [Display(Name = "متن نظر")]
+    public string Text { get; set; }
+
+    [Display(Name = "وضعیت")]
+    public CommentState State { get; set; }
+
+    [Display(Name = "نوع")]
+    public CommentType Type { get; set; }
+
+    [Display(Name = "شناسه محصول/مقاله")]
+    public long OwnerRecordId { get; set; }
+
+    #endregion
+
+    #region Relations
+
+    [Display(Name = "شناسه والد")]
+    public long ParentId { get; set; }
+
+    public Comment Parent { get; set; }
+
+    #endregion
+}
+
+public enum CommentState
+{
+    Canceled,
+    Confirmed
+}
+
+public enum CommentType
+{
+    Product,
+    Article
+}
