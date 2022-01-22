673678e1b5b236bb3c5caf3893f764ad987d7e08
Ali M <alimousavi-01@outlook.com>
2022-01-23 02:47:02 +0330

    Created CommentDbContext

---
+++ src/Modules/CommentModule/Infrastructure/CM.Infrastructure.Persistence/Context/CommentDbContext.cs
@@ -0,0 +1,28 @@
+﻿using _0_Framework.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace IM.Infrastructure.Persistence.Context;
+public class CommentDbContext : DbContext
+{
+    #region Ctor
+
+    public CommentDbContext(DbContextOptions<CommentDbContext> options) : base(options) { }
+
+    #endregion
+
+    public DbSet<CM.Domain.Comment.Comment> Comments { get; set; }
+
+    #region On Model Creating
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplySharedDbContextOnModelCreatingConfiguration();
+
+        var assembly = typeof(CommentDbContext).Assembly;
+        builder.ApplyConfigurationsFromAssembly(assembly);
+
+        base.OnModelCreating(builder);
+    }
+
+    #endregion On Model Creating
+}
