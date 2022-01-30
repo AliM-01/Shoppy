8f3a6924debdaa359f7a40f9a811cff135114bc4
Ali M <alimousavi-01@outlook.com>
2022-01-30 16:31:56 +0330

    Created CommentQueryModel

---
+++ src/01_Shoppy.Query/Contracts/Comment/CommentQueryModel.cs
@@ -0,0 +1,31 @@
+﻿namespace _01_Shoppy.Query.Contracts.Comment;
+
+public class CommentQueryModel
+{
+    [JsonProperty("id")]
+    public long Id { get; set; }
+
+    [Display(Name = "نام")]
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [Display(Name = "ایمیل")]
+    [JsonProperty("email")]
+    public string Email { get; set; }
+
+    [Display(Name = "متن نظر")]
+    [JsonProperty("text")]
+    public string Text { get; set; }
+
+    [Display(Name = "شناسه محصول/مقاله")]
+    [JsonProperty("ownerRecordId")]
+    public long OwnerRecordId { get; set; }
+
+    [Display(Name = "عنوان محصول/مقاله")]
+    [JsonProperty("ownerName")]
+    public string OwnerName { get; set; }
+
+    [Display(Name = "تاریخ ثبت")]
+    [JsonProperty("creationDate")]
+    public string CreationDate { get; set; }
+}
--- src/Modules/CommentModule/Infrastructure/CM.Infrastructure.Shared/Mappings/CommentModuleMappingProfile.cs
+++ src/Modules/CommentModule/Infrastructure/CM.Infrastructure.Shared/Mappings/CommentModuleMappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using _0_Framework.Application.Extensions;
+using _01_Shoppy.Query.Contracts.Comment;
 using AutoMapper;
 using CM.Application.Contracts.Comment.DTOs;
 using CM.Domain.Comment;
@@ -22,6 +23,14 @@ public class CommentModuleMappingProfile : Profile
         #region Add Comment Dto
 
         CreateMap<AddCommentDto, Comment>();
+
+        #endregion
+
+        #region Comment Query Model
+
+        CreateMap<Comment, CommentQueryModel>()
+            .ForMember(dest => dest.CreationDate,
+                opt => opt.MapFrom(src => src.CreationDate.ToDetailedShamsi()));
 
         #endregion
 
