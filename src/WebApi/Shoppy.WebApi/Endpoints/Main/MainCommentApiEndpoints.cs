55c27ee8d58da98098c7c28a3cb292af867d0a31
Ali M <alimousavi-01@outlook.com>
2022-01-30 16:18:01 +0330

    Created MainCommentApiEndpoints

---
+++ src/WebApi/Shoppy.WebApi/Endpoints/Main/MainCommentApiEndpoints.cs
@@ -0,0 +1,11 @@
+﻿namespace Shoppy.WebApi.Endpoints.Main;
+
+public class MainCommentApiEndpoints
+{
+    private const string BaseMainCommentRoute = BaseApiEndpointRoutes.BaseRoute + "/comment";
+
+    public static class Comment
+    {
+        public const string AddComment = BaseMainCommentRoute + "/add";
+    }
+}
