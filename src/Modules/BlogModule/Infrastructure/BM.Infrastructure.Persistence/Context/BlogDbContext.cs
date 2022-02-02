6f5ba2cf0473b8ef0247c9b03580bb3f08c83656
Ali M <alimousavi-01@outlook.com>
2022-02-02 19:00:09 +0330

    Initialized BM.Infrastructure.Persistence Project

---
+++ src/Modules/BlogModule/Infrastructure/BM.Infrastructure.Persistence/Context/BlogDbContext.cs
@@ -0,0 +1,28 @@
+﻿using _0_Framework.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Infrastructure.Persistence.Context;
+public class BlogDbContext : DbContext
+{
+    #region Ctor
+
+    public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options) { }
+
+    #endregion
+
+    public DbSet<BM.Domain.ArticleCategory.ArticleCategory> ArticleCategories { get; set; }
+
+    #region On Model Creating
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplySharedDbContextOnModelCreatingConfiguration();
+
+        var assembly = typeof(BlogDbContext).Assembly;
+        builder.ApplyConfigurationsFromAssembly(assembly);
+
+        base.OnModelCreating(builder);
+    }
+
+    #endregion On Model Creating
+}
