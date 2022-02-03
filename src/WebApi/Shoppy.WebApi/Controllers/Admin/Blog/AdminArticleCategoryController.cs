4711e033c7ef36f93aa9487eeded5868ff5a6a9e
Ali M <alimousavi-01@outlook.com>
2022-02-03 13:54:13 +0330

    Initialized AdminBlogBlogApiEndpoints

---
+++ src/WebApi/Shoppy.WebApi/Controllers/Admin/Blog/AdminArticleCategoryController.cs
@@ -0,0 +1,6 @@
+﻿namespace Shoppy.WebApi.Controllers.Admin.Blog;
+
+[SwaggerTag("مدیریت دسته بندی مقالات")]
+public class AdminArticleCategoryController : BaseApiController
+{
+}
---
+++ src/WebApi/Shoppy.WebApi/Endpoints/Admin/AdminBlogApiEndpoints.cs
@@ -0,0 +1,8 @@
+﻿namespace Shoppy.WebApi.Endpoints.Admin;
+
+public class AdminBlogBlogApiEndpoints
+{
+    private const string BaseAdminBlogRoute = BaseApiEndpointRoutes.AdminBaseRoute + "/blog";
+
+
+}
