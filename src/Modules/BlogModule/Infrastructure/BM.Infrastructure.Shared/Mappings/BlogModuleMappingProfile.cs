b567a7b94ffb1da21112203b1cb5ba534b59b3c6
Ali M <alimousavi-01@outlook.com>
2022-02-03 13:44:13 +0330

    Created BlogModuleMappingProfile

---
+++ src/Modules/BlogModule/Infrastructure/BM.Infrastructure.Shared/Mappings/BlogModuleMappingProfile.cs
@@ -0,0 +1,47 @@
+﻿using _0_Framework.Application.Extensions;
+using AutoMapper;
+using BM.Application.Contracts.ArticleCategory.DTOs;
+using BM.Domain.ArticleCategory;
+
+namespace BM.Infrastructure.Shared.Mappings;
+
+public class BlogModuleMappingProfile : Profile
+{
+    public BlogModuleMappingProfile()
+    {
+        #region Article Category
+
+        #region Article Category Dto
+
+        CreateMap<ArticleCategory, ArticleCategoryDto>()
+            .ForMember(dest => dest.CreationDate,
+                opt => opt.MapFrom(src => src.CreationDate.ToShamsi()));
+
+        #endregion
+
+        #region Create Article Category
+
+        CreateMap<CreateArticleCategoryDto, ArticleCategory>()
+                       .ForMember(dest => dest.Slug,
+                           opt => opt.MapFrom(src => src.Title.ToSlug()));
+
+        #endregion
+
+        #region Edit Article Category
+
+        CreateMap<ArticleCategory, EditArticleCategoryDto>();
+
+        CreateMap<EditArticleCategoryDto, ArticleCategory>()
+            .ForMember(dest => dest.Id,
+                opt => opt.Ignore())
+            .ForMember(dest => dest.ImagePath,
+                opt => opt.Ignore())
+            .ForMember(dest => dest.Slug,
+                opt => opt.MapFrom(src => src.Title.ToSlug()));
+
+        #endregion
+
+        #endregion
+
+    }
+}
