25a4682479d277ecd719a9065441c435c81aa762
Ali M <alimousavi-01@outlook.com>
2022-02-13 17:40:13 +0330

    Created ArticleCategoryForSelectListDto

---
+++ src/Modules/BlogModule/Application.Contracts/BM.Application.Contracts/ArticleCategory/DTOs/ArticleCategoryForSelectListDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BM.Application.Contracts.ArticleCategory.DTOs;
+
+public class ArticleCategoryForSelectListDto
+{
+    [JsonProperty("id")]
+    public long Id { get; set; }
+
+    [Display(Name = "عنوان")]
+    [JsonProperty("title")]
+    public string Title { get; set; }
+}
