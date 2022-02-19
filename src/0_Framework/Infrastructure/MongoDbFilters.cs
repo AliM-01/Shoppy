43d827b5e61d8fda706176247acfa40b6a80016a
Ali M <alimousavi-01@outlook.com>
2022-02-19 16:46:49 +0330

    Created GetByIdFilterDefinition

---
+++ src/0_Framework/Infrastructure/MongoDbFilters.cs
@@ -0,0 +1,12 @@
+﻿using _0_Framework.Domain;
+using MongoDB.Driver;
+
+namespace _0_Framework.Infrastructure;
+
+public static class MongoDbFilters<T> where T : EntityBase
+{
+    public static FilterDefinition<T> GetByIdFilter(string id)
+    {
+        return Builders<T>.Filter.Eq(x => x.Id, id);
+    }
+}
