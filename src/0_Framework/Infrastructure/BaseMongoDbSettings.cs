e1c2e71f6292608336a72d7195180fcba6c97e1a
Ali M <alimousavi-01@outlook.com>
2022-02-19 18:27:18 +0330

    Created BaseMongoDbSettings

---
+++ src/0_Framework/Infrastructure/BaseMongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace _0_Framework.Infrastructure;
+
+public abstract class BaseMongoDbSettings
+{
+    public string ConnectionString { get; set; }
+
+    public string DbName { get; set; }
+}
