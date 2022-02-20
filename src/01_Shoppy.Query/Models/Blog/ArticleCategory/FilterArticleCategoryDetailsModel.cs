770d2af4e7f4e63c57c74b12104f899ae5697eaa
Ali M <alimousavi-01@outlook.com>
2022-02-20 18:26:57 +0330

    Created FilterArticleCategoryDetailsModel

---
+++ src/01_Shoppy.Query/Models/Blog/ArticleCategory/FilterArticleCategoryDetailsModel.cs
@@ -0,0 +1,51 @@
+﻿using _0_Framework.Application.Models.Paging;
+using _01_Shoppy.Query.Models.Blog.Article;
+
+namespace _01_Shoppy.Query.Models.Blog.ArticleCategory;
+
+public class FilterArticleCategoryDetailsModel : BasePaging
+{
+    #region Properties
+
+    [Display(Name = "اسلاگ دسته بندی")]
+    [JsonProperty("slug")]
+    public string Slug { get; set; }
+
+    [Display(Name = "مقالات")]
+    [JsonProperty("articles")]
+    public IEnumerable<ArticleQueryModel> Articles { get; set; }
+
+    #endregion
+
+    #region Methods
+
+    public FilterArticleCategoryDetailsModel SetData(IEnumerable<ArticleQueryModel> articles)
+    {
+        this.Articles = articles;
+        return this;
+    }
+
+    public FilterArticleCategoryDetailsModel SetPaging(BasePaging paging)
+    {
+        this.PageId = paging.PageId;
+        this.AllPagesCount = paging.AllPagesCount;
+        this.StartPage = paging.StartPage;
+        this.EndPage = paging.EndPage;
+        this.ShownPages = paging.ShownPages;
+        this.SkipPage = paging.SkipPage;
+        this.TakePage = paging.TakePage;
+        this.PageCount = paging.PageCount;
+        return this;
+    }
+
+    #endregion
+}
+
+public class ArticleCategoryDetailsQueryModel
+{
+    [JsonProperty("articleCategory")]
+    public ArticleCategoryQueryModel ArticleCategory { get; set; }
+
+    [JsonProperty("filterData")]
+    public FilterArticleCategoryDetailsModel FilterData { get; set; }
+}
