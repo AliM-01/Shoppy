0367b6cdc8a2d08d2ec5236f30d1f7bed5c0ce5c
Ali M <alimousavi-01@outlook.com>
2022-02-20 18:40:04 +0330

    Implemented GetArticleDetailsEndpoint

--- src/WebApi/Shoppy.WebApi/Controllers/Main/Blog/ArticleController.cs
+++ src/WebApi/Shoppy.WebApi/Controllers/Main/Blog/ArticleController.cs
@@ -1,10 +1,25 @@
 ﻿using _01_Shoppy.Query.Queries.Article;
+using _01_Shoppy.Query.Queries.Blog.Article;
 
 namespace Shoppy.WebApi.Controllers.Main.Article;
 
 [SwaggerTag("مقاله ها")]
 public class ArticleController : BaseApiController
 {
+    #region Get Article Details
+
+    [HttpGet(MainBlogApiEndpoints.Article.GetArticleDetails)]
+    [SwaggerOperation(Summary = "دریافت جزییات مقاله", Tags = new[] { "Article" })]
+    [SwaggerResponse(200, "success")]
+    [SwaggerResponse(404, "not-found")]
+    public async Task<IActionResult> GetArticleDetails([FromRoute] string slug)
+    {
+        var res = await Mediator.Send(new GetArticleDetailsQuery(slug));
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
     #region Get Latest Articles
 
     [HttpGet(MainBlogApiEndpoints.Article.GetLatestArticles)]
