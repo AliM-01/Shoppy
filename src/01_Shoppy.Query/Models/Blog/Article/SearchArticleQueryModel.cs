0aee2f8d4d3f61feb863ac10aed972e820feed37
Ali M <alimousavi-01@outlook.com>
2022-02-25 10:23:09 +0330

    Created SearchArticleQueryModel

---
+++ src/01_Shoppy.Query/Models/Blog/Article/SearchArticleQueryModel.cs
@@ -0,0 +1,44 @@
+﻿using _0_Framework.Application.Models.Paging;
+
+namespace _01_Shoppy.Query.Models.Blog.Article;
+
+public class SearchArticleQueryModel : BasePaging
+{
+    #region Properties
+
+    [Display(Name = "دسته بندی های انتخاب شده")]
+    [JsonProperty("selectedCategories")]
+    public List<string> SelectedCategories { get; set; }
+
+    [Display(Name = "متن جستجو")]
+    [JsonProperty("phrase")]
+    public string Phrase { get; set; }
+
+    [Display(Name = "مقالات")]
+    [JsonProperty("articles")]
+    public IEnumerable<ArticleQueryModel> Articles { get; set; }
+    #endregion
+
+    #region Methods
+
+    public SearchArticleQueryModel SetData(IEnumerable<ArticleQueryModel> articles)
+    {
+        this.Articles = articles;
+        return this;
+    }
+
+    public SearchArticleQueryModel SetPaging(BasePaging paging)
+    {
+        this.PageId = paging.PageId;
+        this.AllPagesCount = paging.AllPagesCount;
+        this.StartPage = paging.StartPage;
+        this.EndPage = paging.EndPage;
+        this.ShownPages = paging.ShownPages;
+        this.SkipPage = paging.SkipPage;
+        this.TakePage = paging.TakePage;
+        this.PageCount = paging.PageCount;
+        return this;
+    }
+
+    #endregion
+}
