f88f3baf618df79c22710cae01f256d7cce45aaf
Ali M <alimousavi-01@outlook.com>
2022-02-25 10:34:08 +0330

    Created MenuDto

---
+++ src/01_Shoppy.Query/Models/Common/MenuDto.cs
@@ -0,0 +1,11 @@
+﻿using _01_Shoppy.Query.Models.Blog.ArticleCategory;
+using _01_Shoppy.Query.Models.ProductCategory;
+
+namespace _01_Shoppy.Query.Models.Common;
+
+public class MenuDto
+{
+    public List<ProductCategoryQueryModel> ProductCategories { get; set; }
+
+    public List<ArticleCategoryQueryModel> ArticleCategories { get; set; }
+}
