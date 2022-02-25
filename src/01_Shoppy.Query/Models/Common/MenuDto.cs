699cf72b1d6e9e89b6142422822184b496fe78b9
Ali M <alimousavi-01@outlook.com>
2022-02-25 10:38:47 +0330

    Implemented GetMenuQuery

--- src/01_Shoppy.Query/Models/Common/MenuDto.cs
+++ src/01_Shoppy.Query/Models/Common/MenuDto.cs
@@ -5,6 +5,12 @@ namespace _01_Shoppy.Query.Models.Common;
 
 public class MenuDto
 {
+    public MenuDto(List<ProductCategoryQueryModel> productCategories, List<ArticleCategoryQueryModel> articleCategories)
+    {
+        ProductCategories = productCategories;
+        ArticleCategories = articleCategories;
+    }
+
     public List<ProductCategoryQueryModel> ProductCategories { get; set; }
 
     public List<ArticleCategoryQueryModel> ArticleCategories { get; set; }
---
+++ src/01_Shoppy.Query/Queries/Common/GetMenuQuery.cs
@@ -0,0 +1,42 @@
+﻿using _0_Framework.Infrastructure;
+using _01_Shoppy.Query.Models.Blog.ArticleCategory;
+using _01_Shoppy.Query.Models.Common;
+using _01_Shoppy.Query.Models.ProductCategory;
+
+namespace _01_Shoppy.Query.Queries.Common;
+
+public record GetMenuQuery() : IRequest<Response<MenuDto>>;
+
+public class GetMenuQueryHandler : IRequestHandler<GetMenuQuery, Response<MenuDto>>
+{
+    #region Ctor
+
+    private readonly IGenericRepository<SM.Domain.ProductCategory.ProductCategory> _productCategoryRepository;
+    private readonly IGenericRepository<BM.Domain.ArticleCategory.ArticleCategory> _articleCategoryRepository;
+    private readonly IMapper _mapper;
+
+    public GetMenuQueryHandler(IGenericRepository<SM.Domain.ProductCategory.ProductCategory> productCategoryRepository,
+                               IGenericRepository<BM.Domain.ArticleCategory.ArticleCategory> articleCategoryRepository,
+                               IMapper mapper)
+    {
+        _productCategoryRepository = Guard.Against.Null(productCategoryRepository, nameof(_productCategoryRepository));
+        _articleCategoryRepository = Guard.Against.Null(articleCategoryRepository, nameof(_articleCategoryRepository));
+        _mapper = Guard.Against.Null(mapper, nameof(_mapper));
+    }
+
+    #endregion
+
+
+    public async Task<Response<MenuDto>> Handle(GetMenuQuery request, CancellationToken cancellationToken)
+    {
+        var productCategories = (await _productCategoryRepository.AsQueryable().ToListAsyncSafe())
+             .Select(productCategory => _mapper.Map(productCategory, new ProductCategoryQueryModel()))
+             .ToList();
+
+        var articleCategories = (await _articleCategoryRepository.AsQueryable().ToListAsyncSafe())
+             .Select(articleCategory => _mapper.Map(articleCategory, new ArticleCategoryQueryModel()))
+             .ToList();
+
+        return new Response<MenuDto>(new MenuDto(productCategories, articleCategories));
+    }
+}
