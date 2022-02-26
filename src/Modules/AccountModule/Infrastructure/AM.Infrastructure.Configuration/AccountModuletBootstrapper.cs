02638c3133d506a57e2b8f6871adaf9f79ed9dae
Ali M <alimousavi-01@outlook.com>
2022-02-26 16:39:11 +0330

    Created JwtSettings

--- src/Modules/AccountModule/Infrastructure/AM.Infrastructure.Configuration/AccountModuletBootstrapper.cs
+++ src/Modules/AccountModule/Infrastructure/AM.Infrastructure.Configuration/AccountModuletBootstrapper.cs
@@ -2,9 +2,14 @@
 using AM.Infrastructure.Persistence.Seed;
 using AM.Infrastructure.Persistence.Settings;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 using System;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace AM.Infrastructure.Configuration;
@@ -13,6 +18,8 @@ public class AccountModuletBootstrapper
 {
     public static async Task ConfigureAsync(IServiceCollection services, Microsoft.Extensions.Configuration.IConfiguration config)
     {
+        #region db config
+
         services.Configure<AccountDbSettings>(config.GetSection("AccountDbSettings"));
 
         services.AddIdentity<Account, AccountRole>()
@@ -23,11 +30,11 @@ public class AccountModuletBootstrapper
 
         services.AddMediatR(typeof(AccountModuletBootstrapper).Assembly);
 
-        using (var scope = services.BuildServiceProvider())
+        using (var sp = services.BuildServiceProvider())
         {
             try
             {
-                var roleManager = scope.GetRequiredService<RoleManager<AccountRole>>();
+                var roleManager = sp.GetRequiredService<RoleManager<AccountRole>>();
                 await SeedDefaultRoles.SeedAsync(roleManager);
 
             }
@@ -36,5 +43,68 @@ public class AccountModuletBootstrapper
                 throw;
             }
         }
+
+        #endregion
+
+        #region auth config
+
+        services.Configure<JwtSettings>(config.GetSection("JwtSettings"));
+
+        var jwtSettings = services.BuildServiceProvider().CreateScope().ServiceProvider.GetRequiredService<JwtSettings>();
+
+        services.AddAuthentication("OAuth")
+            .AddJwtBearer("OAuth", c =>
+            {
+                var secretBytes = Encoding.UTF8.GetBytes(jwtSettings.Secret);
+                var key = new SymmetricSecurityKey(secretBytes);
+
+                c.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        context.Response.StatusCode = 403;
+                        context.Response.ContentType = "application/json";
+                        var result = JsonConvert.SerializeObject(new
+                        {
+                            status = "un-authorized",
+                            message = context.Exception.Message.ToString()
+                        }); ;
+                        return context.Response.WriteAsync(result);
+                    },
+                    OnChallenge = context =>
+                    {
+                        context.HandleResponse();
+                        context.Response.StatusCode = 401;
+                        context.Response.ContentType = "application/json";
+                        var result = JsonConvert.SerializeObject(new
+                        {
+                            status = "un-authorized",
+                            message = "لطفا به حساب کاربری خود وارد شوید"
+                        });
+                        return context.Response.WriteAsync(result);
+                    },
+                    OnForbidden = context =>
+                    {
+                        context.Response.StatusCode = 403;
+                        context.Response.ContentType = "application/json";
+                        var result = JsonConvert.SerializeObject(new
+                        {
+                            status = "un-authorized",
+                            message = "لطفا به حساب کاربری خود وارد شوید"
+                        });
+                        return context.Response.WriteAsync(result);
+                    },
+                };
+
+                c.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ClockSkew = TimeSpan.Zero,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audiance,
+                    IssuerSigningKey = key,
+                };
+            });
+
+        #endregion
     }
 }
