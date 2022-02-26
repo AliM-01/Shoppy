f6ad83b2e0660448c574c93ef381734547f03339
Ali M <alimousavi-01@outlook.com>
2022-02-26 15:57:14 +0330

    Created SeedDefaultRoles

---
+++ src/Modules/AccountModule/Infrastructure/AM.Infrastructure.Persistence/Seed/SeedDefaultRoles.cs
@@ -0,0 +1,23 @@
+﻿using AM.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace AM.Infrastructure.Persistence.Seed;
+
+public static class SeedDefaultRoles
+{
+    public static async Task SeedAsync(RoleManager<IdentityRole> roleManager)
+    {
+        //Seed Roles
+
+        if (!await roleManager.RoleExistsAsync(Roles.Admin.ToString()))
+        {
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+        }
+
+        if (!await roleManager.RoleExistsAsync(Roles.BasicUser.ToString()))
+        {
+            await roleManager.CreateAsync(new IdentityRole(Roles.BasicUser.ToString()));
+        }
+    }
+}
+
