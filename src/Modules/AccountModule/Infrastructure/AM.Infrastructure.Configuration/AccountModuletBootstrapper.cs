327cfc149c2766812a53d4c78c42b2ee4ba7fe52
Ali M <alimousavi-01@outlook.com>
2022-02-26 15:46:29 +0330

    Created AccountModuletBootstrapper

---
+++ src/Modules/AccountModule/Infrastructure/AM.Infrastructure.Configuration/AccountModuletBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using AM.Domain.Account;
+using AM.Infrastructure.Persistence.Settings;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AM.Infrastructure.Configuration;
+
+public class AccountModuletBootstrapper
+{
+    public static void Configure(IServiceCollection services, Microsoft.Extensions.Configuration.IConfiguration config)
+    {
+        services.Configure<AccountDbSettings>(config.GetSection("AccountDbSettings"));
+
+        var mongoDbSettings = services.BuildServiceProvider().GetRequiredService<AccountDbSettings>();
+
+        services.AddIdentity<Account, AccountRole>()
+            .AddMongoDbStores<Account, AccountRole, Guid>
+            (
+                mongoDbSettings.ConnectionString, mongoDbSettings.DbName
+            );
+
+        services.AddMediatR(typeof(AccountModuletBootstrapper).Assembly);
+    }
+}
