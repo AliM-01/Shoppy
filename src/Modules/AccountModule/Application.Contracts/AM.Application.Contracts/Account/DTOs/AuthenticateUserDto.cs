68f5cd4824d96673fe0359afe1322c4dc344408b
Ali M <alimousavi-01@outlook.com>
2022-02-26 15:18:58 +0330

    Added AuthenticateUserDto

---
+++ src/Modules/AccountModule/Application.Contracts/AM.Application.Contracts/Account/DTOs/AuthenticateUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AM.Application.Contracts.Account.DTOs;
+
+public class AuthenticateUserDto
+{
+    [JsonProperty("mobile")]
+    [Display(Name = "موبایل")]
+    public string Mobile { get; set; }
+
+    [JsonProperty("password")]
+    [Display(Name = "رمز عبور")]
+    public string Password { get; set; }
+}
