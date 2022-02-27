e53926c23b2eac225a43921e7b15e01a0425a102
Ali M <alimousavi-01@outlook.com>
2022-02-27 19:10:18 +0330

    Bug Fixes

---
+++ src/Modules/AccountModule/Application.Contracts/AM.Application.Contracts/Common/Settings/BearerTokenSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace AM.Application.Contracts.Common.Settings;
+
+public class BearerTokenSettings
+{
+    public string Key { set; get; }
+    public string Issuer { set; get; }
+    public string Audience { set; get; }
+    public int AccessTokenExpirationMinutes { set; get; }
+    public int RefreshTokenExpirationMinutes { set; get; }
+    public bool AllowMultipleLoginsFromTheSameUser { set; get; }
+    public bool AllowSignoutAllUserActiveClients { set; get; }
+}
