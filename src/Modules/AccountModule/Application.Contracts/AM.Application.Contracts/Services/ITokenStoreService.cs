6225d5fbc36406e605bc998d28c9ffffe91c9bc7
Ali M <alimousavi-01@outlook.com>
2022-02-27 19:43:47 +0330

    Created ITokenStoreService

---
+++ src/Modules/AccountModule/Application.Contracts/AM.Application.Contracts/Services/ITokenStoreService.cs
@@ -0,0 +1,24 @@
+﻿using AM.Domain.Account;
+
+namespace AM.Application.Contracts.Services;
+
+public interface ITokenStoreService
+{
+    Task AddUserToken(UserToken userToken);
+
+    Task AddUserToken(Domain.Account.Account user, string refreshTokenSerial, string accessToken, string refreshTokenSourceSerial);
+
+    Task<bool> IsValidToken(string accessToken, int userId);
+
+    Task DeleteExpiredTokens();
+
+    Task<UserToken> FindToken(string refreshTokenValue);
+
+    Task DeleteToken(string refreshTokenValue);
+
+    Task DeleteTokensWithSameRefreshTokenSource(string refreshTokenIdHashSource);
+
+    Task InvalidateUserTokens(int userId);
+
+    Task RevokeUserBearerTokens(string userIdValue, string refreshTokenValue);
+}
