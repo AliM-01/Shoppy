9dff8539313a8d97289d7d8632e1b89dc881cc21
Ali M <alimousavi-01@outlook.com>
2022-02-27 19:11:06 +0330

    Created JwtTokenResponse

---
+++ src/Modules/AccountModule/Application.Contracts/AM.Application.Contracts/Common/JwtTokenResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace AM.Application.Contracts.Common;
+
+public class JwtTokenResponse
+{
+    public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
+    public string RefreshTokenSerial { get; set; }
+    public IEnumerable<Claim> Claims { get; set; }
+}
