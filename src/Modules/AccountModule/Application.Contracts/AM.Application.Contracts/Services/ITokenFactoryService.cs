bb0a2a52e133d543cd353ab1b28b9bdb623fbd0b
Ali M <alimousavi-01@outlook.com>
2022-02-27 19:12:02 +0330

    Created ITokenFactoryService

---
+++ src/Modules/AccountModule/Application.Contracts/AM.Application.Contracts/Services/ITokenFactoryService.cs
@@ -0,0 +1,10 @@
+﻿using AM.Application.Contracts.Common;
+
+namespace AM.Application.Contracts.Services;
+
+public interface ITokenFactoryService
+{
+    Task<JwtTokenResponse> CreateJwtTokensAsync(Domain.Account.Account user);
+
+    string GetRefreshTokenSerial(string refreshTokenValue);
+}
