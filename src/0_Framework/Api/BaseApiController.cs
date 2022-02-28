44e6716d682201209b5fa38929b6235d53295aa4
Ali M <alimousavi-01@outlook.com>
2022-02-28 18:22:27 +0330

    Created BaseAdminApiController

--- src/0_Framework/Api/BaseApiController.cs
+++ src/0_Framework/Api/BaseApiController.cs
@@ -1,11 +1,23 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace _0_Framework.Api;
 
 [ApiController]
+[EnableCors("CorsPolicy")]
 public abstract class BaseApiController : ControllerBase
+{
+    private IMediator _mediator;
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+}
+
+[ApiController]
+[EnableCors("CorsPolicy")]
+[Authorize]
+public abstract class BaseAdminApiController : ControllerBase
 {
     private IMediator _mediator;
     protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
