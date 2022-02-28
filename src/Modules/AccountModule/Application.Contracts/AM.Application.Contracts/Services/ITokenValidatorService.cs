5d35056b5ceab3d101e071ef2fd6cc8a4eab4ba4
Ali M <alimousavi-01@outlook.com>
2022-02-28 18:15:33 +0330

    Created ITokenValidatorService

---
+++ src/Modules/AccountModule/Application.Contracts/AM.Application.Contracts/Services/ITokenValidatorService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace AM.Application.Contracts.Services;
+
+public interface ITokenValidatorService
+{
+    Task ValidateAsync(TokenValidatedContext context);
+}
