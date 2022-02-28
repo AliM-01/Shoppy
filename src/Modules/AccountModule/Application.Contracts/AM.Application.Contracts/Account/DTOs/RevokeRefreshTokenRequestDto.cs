5fa17dad37e995a2b7dd94e1c72abd1daacf2f80
Ali M <alimousavi-01@outlook.com>
2022-02-28 14:52:34 +0330

    Created RevokeRefreshTokenCommand

---
+++ src/Modules/AccountModule/Application.Contracts/AM.Application.Contracts/Account/DTOs/RevokeRefreshTokenRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace AM.Application.Contracts.Account.DTOs;
+
+public class RevokeRefreshTokenRequestDto
+{
+    [JsonProperty("refreshToken")]
+    public string RefreshToken { get; set; }
+}
