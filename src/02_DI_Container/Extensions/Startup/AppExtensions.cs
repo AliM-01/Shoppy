414cc698e104752839b18bfc40cbeebd718e0ab2
Ali M <alimousavi-01@outlook.com>
2022-02-28 18:59:09 +0330

    Modified UseSwaggerExtension

--- src/02_DI_Container/Extensions/Startup/AppExtensions.cs
+++ src/02_DI_Container/Extensions/Startup/AppExtensions.cs
@@ -14,6 +14,11 @@ public static class AppExtensions
             c.DisplayRequestDuration();
             c.SwaggerEndpoint("/swagger/v1/swagger.json", title);
             c.InjectStylesheet("/swagger-ui/css/custom.css");
+            c.DefaultModelExpandDepth(2);
+            c.DefaultModelRendering(Swashbuckle.AspNetCore.SwaggerUI.ModelRendering.Model);
+            c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+            c.EnableDeepLinking();
+            c.DisplayOperationId();
         });
     }
 
