0aaf9b612e88a43da2283cd372d8efca273d7e55
Ali M <alimousavi-01@outlook.com>
2022-02-28 19:02:59 +0330

    Created CustonJsonConverter

---
+++ src/0_Framework/Api/CustonJsonConverter.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace _0_Framework.Api;
+
+public static class CustonJsonConverter
+{
+    public static string Serialize(object? value)
+    {
+        return JsonConvert.SerializeObject(value, Formatting.Indented);
+    }
+}
