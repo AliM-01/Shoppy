e9164ff5a591f4c5ca2c14ab5e961f4191dd298c
Ali M <alimousavi-01@outlook.com>
2022-03-03 19:32:04 +0330

    Created AdminAccountController

---
+++ src/WebApi/Shoppy.WebApi/Controllers/Admin/Account/AdminAccountController.cs
@@ -0,0 +1,69 @@
+﻿using AM.Application.Contracts.Account.Commands;
+using AM.Application.Contracts.Account.DTOs;
+using AM.Application.Contracts.Account.Queries;
+
+namespace Shoppy.WebApi.Controllers.Admin.Blog;
+
+[SwaggerTag("مدیریت کاربران")]
+public class AdminAccountController : BaseAdminApiController
+{
+    #region Filter Accounts
+
+    [HttpGet(AdminAccountApiEndpoints.Account.FilterAccounts)]
+    [SwaggerOperation(Summary = "فیلتر  مقالات", Tags = new[] { "AdminAccount" })]
+    [SwaggerResponse(200, "success")]
+    public async Task<IActionResult> FilterAccounts([FromQuery] FilterAccountDto filter)
+    {
+        var res = await Mediator.Send(new FilterAccountsQuery(filter));
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
+
+    #region Get Account Details
+
+    [HttpGet(AdminAccountApiEndpoints.Account.GetAccountDetails)]
+    [SwaggerOperation(Summary = "دریافت جزییات  کاربر", Tags = new[] { "AdminAccount" })]
+    [SwaggerResponse(200, "success")]
+    [SwaggerResponse(404, "not-found")]
+    public async Task<IActionResult> GetAccountDetails([FromRoute] Guid id)
+    {
+        var res = await Mediator.Send(new GetAccountDetailsQuery(id.ToString()));
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
+
+    #region Edit Account 
+
+    [HttpPut(AdminAccountApiEndpoints.Account.EditAccount)]
+    [SwaggerOperation(Summary = "ویرایش  کاربر", Tags = new[] { "AdminAccount" })]
+    [SwaggerResponse(201, "success : created")]
+    [SwaggerResponse(400, "error : title is duplicated")]
+    [SwaggerResponse(404, "not-found")]
+    public async Task<IActionResult> EditAccount([FromForm] EditAccountDto editRequest)
+    {
+        var res = await Mediator.Send(new EditAccountCommand(editRequest));
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
+
+    #region DeActivate Account 
+
+    [HttpDelete(AdminAccountApiEndpoints.Account.DeActivateAccount)]
+    [SwaggerOperation(Summary = "حذف  کاربر", Tags = new[] { "AdminAccount" })]
+    [SwaggerResponse(201, "success : created")]
+    [SwaggerResponse(404, "not-found")]
+    public async Task<IActionResult> DeleteAccount([FromRoute] Guid id)
+    {
+        var res = await Mediator.Send(new DeActivateAccountCommand(id.ToString()));
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
+}
--- src/WebApi/Shoppy.WebApi/Endpoints/Admin/AdminAccountApiEndpoints.cs
+++ src/WebApi/Shoppy.WebApi/Endpoints/Admin/AdminAccountApiEndpoints.cs
@@ -4,4 +4,16 @@ public class AdminAccountApiEndpoints
 {
     private const string BaseAdminAccountRoute = BaseApiEndpointRoutes.AdminBaseRoute + "/account";
 
+    public static class Account
+    {
+        public const string FilterAccounts = BaseAdminAccountRoute + "/filter";
+
+        public const string GetAccountDetails = BaseAdminAccountRoute + "/{id}";
+
+        public const string EditAccount = BaseAdminAccountRoute + "/edit";
+
+        public const string ActivateAccount = BaseAdminAccountRoute + "/activate/{id}";
+
+        public const string DeActivateAccount = BaseAdminAccountRoute + "/de-activate/{id}";
+    }
 }
