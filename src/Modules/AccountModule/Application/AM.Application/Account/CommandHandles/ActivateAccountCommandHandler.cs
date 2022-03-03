59c7f72ea8de08532b3095951b66d027c2aa9263
Ali M <alimousavi-01@outlook.com>
2022-03-03 19:25:37 +0330

    Implemented  DeActivateAccountCommand

---
+++ src/Modules/AccountModule/Application/AM.Application/Account/CommandHandles/ActivateAccountCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using _0_Framework.Application.Exceptions;
+
+namespace AM.Application.Account.CommandHandles;
+
+public class ActivateAccountCommandHandler : IRequestHandler<ActivateAccountCommand, Response<string>>
+{
+    #region Ctor
+
+    private readonly IMapper _mapper;
+    private readonly UserManager<Domain.Account.Account> _userManager;
+
+    public ActivateAccountCommandHandler(IMapper mapper,
+                                         UserManager<Domain.Account.Account> userManager)
+    {
+        _mapper = Guard.Against.Null(mapper, nameof(_mapper));
+        _userManager = Guard.Against.Null(userManager, nameof(_userManager));
+    }
+
+    #endregion Ctor
+
+    public async Task<Response<string>> Handle(ActivateAccountCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userManager.FindByEmailAsync(request.Account.Email);
+
+        if (user is null)
+            throw new ApiException($"کاربری با این ایمیل ${request.Account.Email} پیدا نشد.");
+
+        user.EmailConfirmed = true;
+
+        await _userManager.UpdateAsync(user);
+
+        return new Response<string>("حساب کاربری شما با موفقیت فعال شد");
+    }
+}
---
+++ src/Modules/AccountModule/Application/AM.Application/Account/CommandHandles/DeActivateAccountCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using _0_Framework.Application.Exceptions;
+
+namespace AM.Application.Account.CommandHandles;
+
+public class DeActivateAccountCommandHandler : IRequestHandler<DeActivateAccountCommand, Response<string>>
+{
+    #region Ctor
+
+    private readonly IMapper _mapper;
+    private readonly UserManager<Domain.Account.Account> _userManager;
+
+    public DeActivateAccountCommandHandler(IMapper mapper,
+                                         UserManager<Domain.Account.Account> userManager)
+    {
+        _mapper = Guard.Against.Null(mapper, nameof(_mapper));
+        _userManager = Guard.Against.Null(userManager, nameof(_userManager));
+    }
+
+    #endregion Ctor
+
+    public async Task<Response<string>> Handle(DeActivateAccountCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userManager.FindByIdAsync(request.AccountId);
+
+        if (user is null)
+            throw new NotFoundApiException();
+
+        user.EmailConfirmed = false;
+
+        await _userManager.UpdateAsync(user);
+
+        return new Response<string>();
+    }
+}
