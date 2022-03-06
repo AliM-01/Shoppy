2733a1c54ac861851a1fcc60b255cf18fa5cfc34
Ali M <alimousavi-01@outlook.com>
2022-03-06 17:15:41 +0330

    Created Order Domain Model

---
+++ src/Modules/OrderModule/Domain/OM.Domain/Order/Order.cs
@@ -0,0 +1,48 @@
+﻿using _0_Framework.Domain;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace OM.Domain.Order;
+
+public class Order : EntityBase
+{
+    #region Properties
+
+    [Display(Name = "حساب")]
+    [BsonElement("accountId")]
+    public string AccountId { get; set; }
+
+    [Display(Name = "حساب")]
+    [BsonElement("paymentMethod")]
+    public int PaymentMethod { get; set; }
+
+    [Display(Name = "حساب")]
+    [BsonElement("totalAmount")]
+    public double TotalAmount { get; set; }
+
+    [Display(Name = "حساب")]
+    [BsonElement("discountAmount")]
+    public double DiscountAmount { get; set; }
+
+    [Display(Name = "حساب")]
+    [BsonElement("paymentAmount")]
+    public double PaymentAmount { get; set; }
+
+    [Display(Name = "حساب")]
+    [BsonElement("isPaid")]
+    public bool IsPaid { get; set; }
+
+    [Display(Name = "حساب")]
+    [BsonElement("isCanceled")]
+    public bool IsCanceled { get; set; }
+
+    [Display(Name = "حساب")]
+    [BsonElement("issueTrackingNo")]
+    public string IssueTrackingNo { get; set; }
+
+    [Display(Name = "حساب")]
+    [BsonElement("refId")]
+    public long RefId { get; set; }
+
+    #endregion
+}
