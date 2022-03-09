cc95a0ca220c9accb01ef768275d81e168f4fded
Ali M <alimousavi-01@outlook.com>
2022-03-09 19:47:06 +0330

    Created OrderDbSettings

---
+++ src/Modules/OrderModule/Infrastructure/OM.Infrastructure.Persistence/Settings/OrderDbSettings.cs
@@ -0,0 +1,10 @@
+﻿using _0_Framework.Infrastructure;
+
+namespace OM.Infrastructure.Persistence.Settings;
+
+public class OrderDbSettings : BaseMongoDbSettings
+{
+    public string OrderCollection { get; set; }
+
+    public string OrderItemCollection { get; set; }
+}
