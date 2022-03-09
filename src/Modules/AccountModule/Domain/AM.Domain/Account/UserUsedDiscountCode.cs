319c6dcb581d8007df96da258dc5451138f37245
Ali M <alimousavi-01@outlook.com>
2022-03-09 11:50:03 +0330

    Created UserUsedDiscountCode

---
+++ src/Modules/AccountModule/Domain/AM.Domain/Account/UserUsedDiscountCode.cs
@@ -0,0 +1,15 @@
+﻿using _0_Framework.Domain;
+using _0_Framework.Domain.Attributes;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AM.Domain.Account;
+
+[BsonCollection("userUsedDiscountCodes")]
+public class UserUsedDiscountCode : EntityBase
+{
+    [BsonElement("userId")]
+    public string UserId { get; set; }
+
+    [BsonElement("discountCodeId")]
+    public string DiscountCodeId { get; set; }
+}
