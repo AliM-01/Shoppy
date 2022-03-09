496b2a98e24a5d3184eb54e1d464de9597849834
Ali M <alimousavi-01@outlook.com>
2022-03-09 20:30:08 +0330

    Implemented AdminOrderController

---
+++ src/WebApi/Shoppy.WebApi/Controllers/Admin/Order/AdminOrderController.cs
@@ -0,0 +1,23 @@
+﻿using OM.Application.Contracts.Order.DTOs;
+using OM.Application.Contracts.Order.Queries;
+
+namespace Shoppy.WebApi.Controllers.Admin.Discount;
+
+[SwaggerTag("مدیریت سفارشات")]
+public class AdminOrderController : BaseAdminApiController
+{
+    #region Filter Order
+
+    [HttpGet(AdminOrderApiEndpoints.Order.FilterOrders)]
+    [SwaggerOperation(Summary = "فیلتر سفارشات", Tags = new[] { "AdminOrder" })]
+    [SwaggerResponse(200, "success")]
+    public async Task<IActionResult> FilterInventories([FromQuery] FilterOrderDto filter)
+    {
+        var res = await Mediator.Send(new FilterOrdersQuery(filter));
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
+
+}
---
+++ src/WebApi/Shoppy.WebApi/Endpoints/Admin/AdminOrderApiEndpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace Shoppy.WebApi.Endpoints.Admin;
+
+public class AdminOrderApiEndpoints
+{
+    private const string BaseAdminOrderRoute = BaseApiEndpointRoutes.AdminBaseRoute + "/order";
+
+    public static class Order
+    {
+        private const string BaseOrderRoute = BaseAdminOrderRoute + "/order";
+
+        public const string FilterOrders = BaseOrderRoute + "/filter";
+    }
+}
