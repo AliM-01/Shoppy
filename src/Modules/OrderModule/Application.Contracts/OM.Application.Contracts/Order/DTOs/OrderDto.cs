867c9b011e2c94a26aa74b211099754e4a46c559
Ali M <alimousavi-01@outlook.com>
2022-03-09 12:18:12 +0330

    Created OrderDto

---
+++ src/Modules/OrderModule/Application.Contracts/OM.Application.Contracts/Order/DTOs/OrderDto.cs
@@ -0,0 +1,54 @@
+﻿namespace OM.Application.Contracts.Order.DTOs;
+
+public class OrderDto
+{
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [Display(Name = "شناسه کاربر")]
+    [JsonProperty("accountId")]
+    public string AccountId { get; set; }
+
+    [Display(Name = "نام کاربر")]
+    [JsonProperty("userFullName")]
+    public string UserFullName { get; set; }
+
+    [Display(Name = "روش پرداخت")]
+    [JsonProperty("paymentMethod")]
+    public int PaymentMethod { get; set; }
+
+    [Display(Name = "روش پرداخت")]
+    [JsonProperty("paymentMethodId")]
+    public int PaymentMethodId { get; set; }
+
+    [Display(Name = "حساب")]
+    [JsonProperty("totalAmount")]
+    public double TotalAmount { get; set; }
+
+    [Display(Name = "حساب")]
+    [JsonProperty("discountAmount")]
+    public double DiscountAmount { get; set; }
+
+    [Display(Name = "حساب")]
+    [JsonProperty("paymentAmount")]
+    public double PaymentAmount { get; set; }
+
+    [Display(Name = "حساب")]
+    [JsonProperty("isPaid")]
+    public bool IsPaid { get; set; }
+
+    [Display(Name = "حساب")]
+    [JsonProperty("isCanceled")]
+    public bool IsCanceled { get; set; }
+
+    [Display(Name = "حساب")]
+    [JsonProperty("issueTrackingNo")]
+    public string IssueTrackingNo { get; set; }
+
+    [Display(Name = "حساب")]
+    [JsonProperty("refId")]
+    public long RefId { get; set; }
+
+    [JsonProperty("creationDate")]
+    public DateTime CreationDate { get; set; }
+}
--- src/Modules/OrderModule/Domain/OM.Domain/Order/Order.cs
+++ src/Modules/OrderModule/Domain/OM.Domain/Order/Order.cs
@@ -6,43 +6,37 @@ public class Order : EntityBase
     #region Properties
 
     [Display(Name = "حساب")]
-    [BsonElement("accountId")]
-    public string AccountId { get; set; }
+    [BsonElement("userId")]
+    public string UserId { get; set; }
 
-    [Display(Name = "حساب")]
     [BsonElement("paymentMethod")]
     public int PaymentMethod { get; set; }
 
-    [Display(Name = "حساب")]
     [BsonElement("totalAmount")]
     public double TotalAmount { get; set; }
 
-    [Display(Name = "حساب")]
     [BsonElement("discountAmount")]
     public double DiscountAmount { get; set; }
 
-    [Display(Name = "حساب")]
     [BsonElement("paymentAmount")]
     public double PaymentAmount { get; set; }
 
-    [Display(Name = "حساب")]
     [BsonElement("isPaid")]
     public bool IsPaid { get; set; }
 
-    [Display(Name = "حساب")]
     [BsonElement("isCanceled")]
     public bool IsCanceled { get; set; }
 
-    [Display(Name = "حساب")]
+    [Display(Name = "کد پیگیری")]
     [BsonElement("issueTrackingNo")]
     public string IssueTrackingNo { get; set; }
 
-    [Display(Name = "حساب")]
+    [Display(Name = "کد بازگشت درگاه پرداخت")]
     [BsonElement("refId")]
     public long RefId { get; set; }
 
     [BsonElement("items")]
-    public List<OrderItem> Items { get; private set; }
+    public List<OrderItem> Items { get; set; }
 
     #endregion
 }
