3a947f896f4376afcf4fadd4d610cd63e09b6741
Ali M <alimousavi-01@outlook.com>
2022-03-09 12:05:42 +0330

    Generators GenerateCode Refactor : added int subString

--- src/0_Framework/Application/Extensions/Generators.cs
+++ src/0_Framework/Application/Extensions/Generators.cs
@@ -4,9 +4,9 @@ namespace _0_Framework.Application.Extensions;
 
 public static class Generators
 {
-    public static string GenerateCode()
+    public static string GenerateCode(int subString = 5)
     {
-        return Guid.NewGuid().ToString().ToUpper().Substring(0, 5);
+        return Guid.NewGuid().ToString().ToUpper().Substring(0, subString);
     }
 
     public static string GenerateRandomUsername()
