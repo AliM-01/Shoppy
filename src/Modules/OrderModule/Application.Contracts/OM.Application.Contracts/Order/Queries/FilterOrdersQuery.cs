7965effdd5a9ce66fbe6183afccb57dc46f986e4
Ali M <alimousavi-01@outlook.com>
2022-03-09 15:53:55 +0330

    Created FilterOrdersQuery

---
+++ src/Modules/OrderModule/Application.Contracts/OM.Application.Contracts/Order/Queries/FilterOrdersQuery.cs
@@ -0,0 +1,6 @@
+﻿using OM.Application.Contracts.Order.DTOs;
+
+namespace OM.Application.Contracts.Order.Queries;
+
+public record FilterOrdersQuery
+    (FilterOrderDto Filter) : IRequest<Response<FilterOrderDto>>;
--- src/Modules/OrderModule/Application.Contracts/OM.Application.Contracts/_global_usings.cs
+++ src/Modules/OrderModule/Application.Contracts/OM.Application.Contracts/_global_usings.cs
@@ -1,6 +1,5 @@
-﻿//global using _0_Framework.Application.Wrappers;
-//global using _0_Framework.Domain;
-//global using MediatR;
+﻿global using _0_Framework.Application.Wrappers;
+global using MediatR;
 global using Newtonsoft.Json;
 global using System.ComponentModel.DataAnnotations;
 
