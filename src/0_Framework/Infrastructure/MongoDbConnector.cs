d01984be8fb4cb5baed6ed708e00a640b7067dde
Ali M <alimousavi-01@outlook.com>
2022-03-11 17:50:27 +0330

    Created MongoDbConnector

---
+++ src/0_Framework/Infrastructure/MongoDbConnector.cs
@@ -0,0 +1,28 @@
+﻿using _0_Framework.Domain.Attributes;
+using MongoDB.Driver;
+using System.Linq;
+
+namespace _0_Framework.Infrastructure;
+
+public static class MongoDbConnector
+{
+    public static IMongoCollection<TDocument> Conncet<TDocument>(BaseMongoDbSettings dbSettings)
+    {
+        var mongoSettings = MongoClientSettings.FromConnectionString(dbSettings.ConnectionString);
+        mongoSettings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+        var client = new MongoClient(mongoSettings);
+
+        var database = client.GetDatabase(dbSettings.DbName);
+
+        return database.GetCollection<TDocument>(GetCollectionName(typeof(TDocument)));
+    }
+
+    private static string GetCollectionName(Type documentType)
+    {
+        return ((BsonCollectionAttribute)documentType.GetCustomAttributes(
+                typeof(BsonCollectionAttribute),
+                true)
+            .FirstOrDefault())?.CollectionName;
+    }
+}
