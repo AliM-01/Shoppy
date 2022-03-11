3159ae33825512ecb3db303668f8246dc74b9039
Ali M <alimousavi-01@outlook.com>
2022-03-11 14:06:07 +0330

    Implemented AuthUserExtension

---
+++ src/0_Framework/Api/AuthUserExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace _0_Framework.Api;
+
+public static class AuthUserExtension
+{
+    public static string GetUserId(this ClaimsPrincipal claim)
+    {
+        var result = claim?.FindFirst(ClaimTypes.NameIdentifier);
+        return result?.Value;
+    }
+
+    public static string GetUserSerialNumber(this ClaimsPrincipal claim)
+    {
+        var result = claim?.FindFirst(ClaimTypes.SerialNumber);
+        return result?.Value;
+    }
+}
