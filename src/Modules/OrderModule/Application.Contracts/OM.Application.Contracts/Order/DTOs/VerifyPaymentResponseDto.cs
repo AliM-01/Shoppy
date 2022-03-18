6fa395a325f64a121f672445447e1d4eff7b777f
Ali M <alimousavi-01@outlook.com>
2022-03-18 19:21:19 +0330

    Created VerifyPaymentResponseDto

---
+++ src/Modules/OrderModule/Application.Contracts/OM.Application.Contracts/Order/DTOs/VerifyPaymentResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OM.Application.Contracts.Order.DTOs;
+
+public class VerifyPaymentResponseDto
+{
+    [JsonProperty("resultMessage")]
+    public string ResultMessage { get; set; }
+
+    [JsonProperty("issueTracking")]
+    public string IssueTracking { get; set; }
+}
