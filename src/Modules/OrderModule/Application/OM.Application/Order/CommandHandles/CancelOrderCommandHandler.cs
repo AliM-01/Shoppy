98e8a597649ddbfa3fe04e5272b8d94f7fdf1c10
Ali M <alimousavi-01@outlook.com>
2022-03-21 17:53:45 +0330

    Implemented CancelOrderCommandHandler

---
+++ src/Modules/OrderModule/Application/OM.Application/Order/CommandHandles/CancelOrderCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using OM.Application.Contracts.Order.Commands;
+
+namespace OM.Application.Order.CommandHandles;
+
+public class CancelOrderCommandHandler : IRequestHandler<CancelOrderCommand, Response<string>>
+{
+    #region Ctor
+
+    private readonly IRepository<Domain.Order.Order> _orderRepository;
+    private readonly IRepository<Domain.Order.OrderItem> _orderItemRepository;
+    private readonly IMapper _mapper;
+
+    public CancelOrderCommandHandler(IRepository<Domain.Order.Order> orderRepository,
+        IRepository<Domain.Order.OrderItem> orderItemRepository,
+                                                    IMapper mapper)
+    {
+        _orderRepository = Guard.Against.Null(orderRepository, nameof(_orderRepository));
+        _orderItemRepository = Guard.Against.Null(orderItemRepository, nameof(_orderItemRepository));
+        _mapper = Guard.Against.Null(mapper, nameof(_mapper));
+    }
+
+    #endregion
+
+    public async Task<Response<string>> Handle(CancelOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = await _orderRepository.GetByIdAsync(request.OrderId);
+
+        if (order is null)
+            throw new NotFoundApiException();
+
+        order.IsCanceled = true;
+
+        await _orderRepository.UpdateAsync(order);
+
+        return new Response<string>(ApplicationErrorMessage.OperationSuccedded);
+    }
+}
