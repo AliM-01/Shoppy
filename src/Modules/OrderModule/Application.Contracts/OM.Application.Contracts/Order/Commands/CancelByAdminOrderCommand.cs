29dac580ae88addaef1dc95db27350abd76c85a5
Ali M <alimousavi-01@outlook.com>
2022-03-21 18:01:35 +0330

    Created CancelByAdminOrderCommand

---
+++ src/Modules/OrderModule/Application.Contracts/OM.Application.Contracts/Order/Commands/CancelByAdminOrderCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace OM.Application.Contracts.Order.Commands;
+
+public record CancelByAdminOrderCommand(string OrderId) : IRequest<Response<string>>;
---
+++ src/Modules/OrderModule/Application/OM.Application/Order/CommandHandles/CancelOrderCommandHandler - Copy.cs
@@ -0,0 +1,41 @@
+﻿using OM.Application.Contracts.Order.Commands;
+
+namespace OM.Application.Order.CommandHandles;
+
+public class CancelByAdminOrderCommandHandler : IRequestHandler<CancelByAdminOrderCommand, Response<string>>
+{
+    #region Ctor
+
+    private readonly IRepository<Domain.Order.Order> _orderRepository;
+    private readonly IRepository<Domain.Order.OrderItem> _orderItemRepository;
+    private readonly IMapper _mapper;
+
+    public CancelByAdminOrderCommandHandler(IRepository<Domain.Order.Order> orderRepository,
+        IRepository<Domain.Order.OrderItem> orderItemRepository,
+                                                    IMapper mapper)
+    {
+        _orderRepository = Guard.Against.Null(orderRepository, nameof(_orderRepository));
+        _orderItemRepository = Guard.Against.Null(orderItemRepository, nameof(_orderItemRepository));
+        _mapper = Guard.Against.Null(mapper, nameof(_mapper));
+    }
+
+    #endregion
+
+    public async Task<Response<string>> Handle(CancelByAdminOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = await _orderRepository.GetByIdAsync(request.OrderId);
+
+        if (order is null)
+            throw new NotFoundApiException();
+
+        if (order.IsPaid)
+            throw new ApiException("سفارش قبلا پرداخت شده و قابل لغو شدن نمی باشد");
+
+        order.IsCanceled = true;
+        order.IssueTrackingNo = "0000-0000";
+
+        await _orderRepository.UpdateAsync(order);
+
+        return new Response<string>(ApplicationErrorMessage.OperationSuccedded);
+    }
+}
