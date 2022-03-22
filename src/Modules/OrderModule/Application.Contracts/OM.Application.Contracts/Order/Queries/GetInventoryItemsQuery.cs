2d9052e4732b4934fbeed9c46b3d40854c4b0382
Ali M <alimousavi-01@outlook.com>
2022-03-22 19:58:00 +0430

    Created GetInventoryItemsQuery

---
+++ src/Modules/OrderModule/Application.Contracts/OM.Application.Contracts/Order/Queries/GetInventoryItemsQuery.cs
@@ -0,0 +1,6 @@
+﻿using OM.Application.Contracts.Order.DTOs;
+
+namespace OM.Application.Contracts.Order.Queries;
+
+public record GetInventoryItemsQuery
+    (string OrderId, string UserId, bool IsAdmin) : IRequest<Response<OrderItemDto[]>>;
