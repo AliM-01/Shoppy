e3a71cf6b9aedc2512c973ca2c30e0e1e2b5c6a2
Ali M <alimousavi-01@outlook.com>
2022-03-23 20:52:00 +0430

    Implemented ProductSales

--- src/WebApi/Shoppy.WebApi/Controllers/Admin/Report/AdminReportController.cs
+++ src/WebApi/Shoppy.WebApi/Controllers/Admin/Report/AdminReportController.cs
@@ -5,7 +5,7 @@ namespace Shoppy.WebApi.Controllers.Admin.Discount;
 [SwaggerTag("گزارشات")]
 public class AdminReportController : BaseAdminApiController
 {
-    #region Filter Order
+    #region Order
 
     [HttpGet(AdminReportApiEndpoints.Orders)]
     [SwaggerOperation(Summary = "گزارش سفارشات", Tags = new[] { "AdminOrder" })]
@@ -13,6 +13,20 @@ public class AdminReportController : BaseAdminApiController
     public async Task<IActionResult> OrdersReport()
     {
         var res = await Mediator.Send(new GetOrdersChartQuery());
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
+
+    #region ProductSales
+
+    [HttpGet(AdminReportApiEndpoints.ProductSales)]
+    [SwaggerOperation(Summary = "گزارش محصولات فروخته شده", Tags = new[] { "AdminOrder" })]
+    [SwaggerResponse(200, "success")]
+    public async Task<IActionResult> ProductSales()
+    {
+        var res = await Mediator.Send(new GetProductsSoldChartQuery());
 
         return JsonApiResult.Success(res);
     }
--- src/WebApi/Shoppy.WebApi/Endpoints/Admin/AdminReportApiEndpoints.cs
+++ src/WebApi/Shoppy.WebApi/Endpoints/Admin/AdminReportApiEndpoints.cs
@@ -5,4 +5,6 @@ public class AdminReportApiEndpoints
     private const string BaseRoute = BaseApiEndpointRoutes.AdminBaseRoute + "/report";
 
     public const string Orders = BaseRoute + "/orders";
+
+    public const string ProductSales = BaseRoute + "/product-sale";
 }
