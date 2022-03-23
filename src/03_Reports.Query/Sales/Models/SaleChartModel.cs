afe30265903395ddd6f000b5730a9b236c3afcb1
Ali M <alimousavi-01@outlook.com>
2022-03-23 20:24:00 +0430

    Implemented GetSalesChartDataQuery

---
+++ src/03_Reports.Query/Sales/Models/SaleChartModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace _03_Reports.Query.Sales.Models;
+public class SaleChartModel
+{
+    public SaleChartModel(int month, int count)
+    {
+        Month = month;
+        Count = count;
+    }
+
+    [Display(Name = "ماه")]
+    [JsonProperty("month")]
+    public int Month { get; set; }
+
+    [Display(Name = "تعداد")]
+    [JsonProperty("count")]
+    public int Count { get; set; }
+}
---
+++ src/03_Reports.Query/Sales/Queries/GetSalesChartDataQuery.cs
@@ -0,0 +1,39 @@
+﻿using _0_Framework.Application.Wrappers;
+using _0_Framework.Infrastructure.IRepository;
+using _03_Reports.Query.Sales.Models;
+using Ardalis.GuardClauses;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace _03_Reports.Query.Sales.Queries;
+
+public record GetSalesChartDataQuery() : IRequest<Response<List<SaleChartModel>>>;
+
+public class GetSalesChartDataQueryHandler : IRequestHandler<GetSalesChartDataQuery, Response<List<SaleChartModel>>>
+{
+    #region Ctor
+
+    private readonly IRepository<OM.Domain.Order.Order> _orderRepository;
+
+    public GetSalesChartDataQueryHandler(IRepository<OM.Domain.Order.Order> orderRepository)
+    {
+        _orderRepository = Guard.Against.Null(orderRepository, nameof(_orderRepository));
+    }
+
+    #endregion
+
+    public async Task<Response<List<SaleChartModel>>> Handle(GetSalesChartDataQuery request, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var data = new List<SaleChartModel>();
+
+        for (int i = 1; i <= 12; i++)
+        {
+            int count = await _orderRepository.AsQueryable().Where(x => x.CreationDate.Month == i).CountAsync();
+            data.Add(new SaleChartModel(i, count));
+        }
+
+        return new Response<List<SaleChartModel>>(data);
+    }
+}
