85d5855db5d76fd52ffeba2c0772a3d0a881489a
Ali M <alimousavi-01@outlook.com>
2022-03-23 20:43:14 +0430

    Implemented GetProductsSoldChartQuery

---
+++ src/03_Reports.Query/Extensions/OrderChartMonth.cs
@@ -0,0 +1,57 @@
+﻿namespace _03_Reports.Query.Extensions;
+
+public static class OrderChartMonth
+{
+    public static List<ChartModel> OrderMonth(this List<ChartModel> value)
+    {
+        foreach (var item in value)
+        {
+            switch (item.Month)
+            {
+                case 1:
+                    item.MonthOrder = 10;
+                    break;
+
+                case 2:
+                    item.MonthOrder = 11;
+                    break;
+
+                case 3:
+                    item.MonthOrder = 12;
+                    break;
+
+                case 4:
+                    item.MonthOrder = 1;
+                    break;
+
+                case 5:
+                    item.MonthOrder = 2;
+                    break;
+
+                case 6:
+                    item.MonthOrder = 3;
+                    break;
+                case 7:
+                    item.MonthOrder = 4;
+                    break;
+                case 8:
+                    item.MonthOrder = 5;
+                    break;
+                case 9:
+                    item.MonthOrder = 6;
+                    break;
+                case 10:
+                    item.MonthOrder = 7;
+                    break;
+                case 11:
+                    item.MonthOrder = 8;
+                    break;
+                case 12:
+                    item.MonthOrder = 9;
+                    break;
+            }
+        }
+
+        return value.OrderBy(x => x.MonthOrder).ToList();
+    }
+}
--- src/03_Reports.Query/Queries/GetOrdersChartQuery.cs
+++ src/03_Reports.Query/Queries/GetOrdersChartQuery.cs
@@ -1,21 +1,16 @@
-﻿using _0_Framework.Application.Wrappers;
-using _0_Framework.Infrastructure.IRepository;
-using _03_Reports.Query.Models;
-using Ardalis.GuardClauses;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace _03_Reports.Query.Queries;
 
 public record GetOrdersChartQuery() : IRequest<Response<List<ChartModel>>>;
 
-public class GetSalesChartDataQueryHandler : IRequestHandler<GetOrdersChartQuery, Response<List<ChartModel>>>
+public class GetOrdersChartQueryHandler : IRequestHandler<GetOrdersChartQuery, Response<List<ChartModel>>>
 {
     #region Ctor
 
     private readonly IRepository<OM.Domain.Order.Order> _orderRepository;
 
-    public GetSalesChartDataQueryHandler(IRepository<OM.Domain.Order.Order> orderRepository)
+    public GetOrdersChartQueryHandler(IRepository<OM.Domain.Order.Order> orderRepository)
     {
         _orderRepository = Guard.Against.Null(orderRepository, nameof(_orderRepository));
     }
@@ -34,56 +29,6 @@ public class GetSalesChartDataQueryHandler : IRequestHandler<GetOrdersChartQuery
             sales.Add(new ChartModel(i, count));
         }
 
-        foreach (var item in sales)
-        {
-            switch (item.Month)
-            {
-                case 1:
-                    item.MonthOrder = 10;
-                    break;
-
-                case 2:
-                    item.MonthOrder = 11;
-                    break;
-
-                case 3:
-                    item.MonthOrder = 12;
-                    break;
-
-                case 4:
-                    item.MonthOrder = 1;
-                    break;
-
-                case 5:
-                    item.MonthOrder = 2;
-                    break;
-
-                case 6:
-                    item.MonthOrder = 3;
-                    break;
-                case 7:
-                    item.MonthOrder = 4;
-                    break;
-                case 8:
-                    item.MonthOrder = 5;
-                    break;
-                case 9:
-                    item.MonthOrder = 6;
-                    break;
-                case 10:
-                    item.MonthOrder = 7;
-                    break;
-                case 11:
-                    item.MonthOrder = 8;
-                    break;
-                case 12:
-                    item.MonthOrder = 9;
-                    break;
-            }
-        }
-
-        sales = sales.OrderBy(x => x.MonthOrder).ToList();
-
-        return new Response<List<ChartModel>>(sales);
+        return new Response<List<ChartModel>>(sales.OrderMonth());
     }
 }
