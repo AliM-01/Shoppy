02ddc5692e257b58111841a0a67d514fff53087f
Ali M <alimousavi-01@outlook.com>
2022-03-26 22:08:12 +0430

    Implemented ExistsProduct in ProductRepositoryExtension

--- src/Modules/ShopModule/Infrastructure/SM.Infrastructure.Shared/RepositoryExtensions/ProductRepositoryExtension.cs
+++ src/Modules/ShopModule/Infrastructure/SM.Infrastructure.Shared/RepositoryExtensions/ProductRepositoryExtension.cs
@@ -1,5 +1,13 @@
-﻿namespace SM.Infrastructure.Shared.RepositoryExtensions;
+﻿using _0_Framework.Infrastructure.IRepository;
+using SM.Domain.Product;
+using System.Threading.Tasks;
+
+namespace SM.Infrastructure.Shared.RepositoryExtensions;
 
 public static class ProductRepositoryExtension
 {
+    public static async Task<bool> ExistsProduct(this IRepository<Product> productRepository, string productId)
+    {
+        return await productRepository.ExistsAsync(p => p.Id == productId);
+    }
 }
