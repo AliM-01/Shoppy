98e83ec671859282d9daccd40365944341cbaac2
Ali M <alimousavi-01@outlook.com>
2022-03-26 21:22:38 +0430

    Implemented ExistsProductDiscount in DMProucAclService

--- src/Modules/DiscountModule/Application.Contracts/DM.Application.Contracts/Sevices/IDMProucAclService.cs
+++ src/Modules/DiscountModule/Application.Contracts/DM.Application.Contracts/Sevices/IDMProucAclService.cs
@@ -1,4 +1,7 @@
-﻿namespace DM.Application.Contracts.Sevices;
+﻿using System.Threading.Tasks;
+
+namespace DM.Application.Contracts.Sevices;
 public interface IDMProucAclService
 {
+    Task<bool> ExistsProductDiscount(string productId);
 }
--- src/Modules/DiscountModule/Infrastructure/DM.Infrastructure.ProductAcl/Services/DMProucAclService.cs
+++ src/Modules/DiscountModule/Infrastructure/DM.Infrastructure.ProductAcl/Services/DMProucAclService.cs
@@ -1,6 +1,36 @@
-﻿using DM.Application.Contracts.Sevices;
+﻿using _0_Framework.Application.Exceptions;
+using _0_Framework.Infrastructure.IRepository;
+using Ardalis.GuardClauses;
+using DM.Application.Contracts.Sevices;
+using SM.Domain.Product;
 
 namespace DM.Infrastructure.ProductAcl.Services;
 public class DMProucAclService : IDMProucAclService
 {
+    #region ctor
+
+    private readonly IRepository<Domain.ProductDiscount.ProductDiscount> _productDiscountRepository;
+    private readonly IRepository<Product> _productRepository;
+
+    public DMProucAclService(IRepository<Domain.ProductDiscount.ProductDiscount> productDiscountRepository,
+         IRepository<Product> productRepository)
+    {
+        _productDiscountRepository = Guard.Against.Null(productDiscountRepository, nameof(_productDiscountRepository));
+        _productRepository = Guard.Against.Null(productRepository, nameof(_productRepository));
+
+    }
+
+    #endregion
+
+    public async Task<bool> ExistsProductDiscount(string productId)
+    {
+        var existsProduct = await _productRepository.ExistsAsync(p => p.Id == productId);
+
+        if (!existsProduct)
+            throw new NotFoundApiException("محصولی با این شناسه پیدا نشد");
+
+        bool existsDiscount = await _productDiscountRepository.ExistsAsync(x => x.ProductId == productId);
+
+        return existsDiscount;
+    }
 }
