d963ec7cad5b336f8476d4c5259a39fd423c3fa3
Ali M <alimousavi-01@outlook.com>
2022-03-29 22:38:08 +0430

    GetFullName in UserManagerExtension

--- src/Modules/AccountModule/Infrastructure/AM.Infrastructure.Shared/RepositoryExtensions/UserManagerExtension.cs
+++ src/Modules/AccountModule/Infrastructure/AM.Infrastructure.Shared/RepositoryExtensions/UserManagerExtension.cs
@@ -1,4 +1,18 @@
-﻿namespace AM.Infrastructure.Shared.RepositoryExtensions;
+﻿using _0_Framework.Application.Exceptions;
+using AM.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace AM.Infrastructure.Shared.RepositoryExtensions;
 public static class UserManagerExtension
 {
+    public static async Task<string> GetFullName(this UserManager<Account> userManager, string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+
+        if (user is null)
+            throw new NotFoundApiException("کاربری با این شناسه پیدا نشد");
+
+        return $"{user.FirstName} {user.LastName}";
+    }
 }
