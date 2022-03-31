c5703fc16c18d978646686c9a78ebd74b6a7800f
Ali M <alimousavi-01@outlook.com>
2022-03-31 16:22:31 +0430

    Implemented IMProuctAclService

---
+++ src/Modules/InventoryModule/Infrastructure/IM.Infrastructure.AccountAcl/IMAccountAclService.cs
@@ -0,0 +1,25 @@
+﻿using AM.Domain.Account;
+using AM.Infrastructure.Shared.RepositoryExtensions;
+using Ardalis.GuardClauses;
+using IM.Application.Contracts.Sevices;
+using Microsoft.AspNetCore.Identity;
+
+namespace IM.Infrastructure.AccountAcl;
+public class IMProuctAclService : IIMAccountAclService
+{
+    #region ctor
+
+    private readonly UserManager<Account> _userManager;
+
+    public IMProuctAclService(UserManager<Account> userManager)
+    {
+        _userManager = Guard.Against.Null(userManager, nameof(_userManager));
+    }
+
+    #endregion
+
+    public async Task<string> GetFullName(string userId)
+    {
+        return await _userManager.GetFullName(userId);
+    }
+}
