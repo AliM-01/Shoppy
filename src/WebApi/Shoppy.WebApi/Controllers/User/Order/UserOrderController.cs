30e37a672622c5cad33b821583d0b9a0ef41b72b
Ali M <alimousavi-01@outlook.com>
2022-04-07 13:21:33 +0430

    Moved User Cart Operations from MainOrderEndpoints to UserOrderEndpoints

--- src/WebApi/Shoppy.WebApi/Controllers/User/Order/UserOrderController.cs
+++ src/WebApi/Shoppy.WebApi/Controllers/User/Order/UserOrderController.cs
@@ -1,6 +1,82 @@
-﻿namespace Shoppy.WebApi.Controllers.User.Order;
+﻿using OM.Application.Contracts.Order.Commands;
+using OM.Application.Contracts.Order.DTOs;
+
+namespace Shoppy.WebApi.Controllers.User.Order;
 
 [SwaggerTag("سفارشات کاربر")]
 public class UserOrderController : BaseUserApiController
 {
+    #region PlaceOrder
+
+    [HttpPost(UserOrderEndpoints.Order.PlaceOrder)]
+    [SwaggerOperation(Summary = "ثبت سفارش", Tags = new[] { "Order" })]
+    [SwaggerResponse(200, "success")]
+    [ProducesResponseType(typeof(Response<PlaceOrderResponseDto>), 200)]
+    public async Task<IActionResult> PlaceOrder([FromBody] CartDto cart)
+    {
+        var res = await Mediator.Send(new PlaceOrderCommand(cart,
+                                                            User.GetUserId()));
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
+
+    #region CancelOrder
+
+    [HttpDelete(UserOrderEndpoints.Order.CancelOrder)]
+    [SwaggerOperation(Summary = "لفو سفارش", Tags = new[] { "Order" })]
+    [SwaggerResponse(200, "success")]
+    [SwaggerResponse(404, "not-found")]
+    [ProducesResponseType(typeof(Response<string>), 200)]
+    [ProducesResponseType(typeof(Response<string>), 404)]
+    public async Task<IActionResult> CancelOrder([FromRoute] string orderId)
+    {
+        var res = await Mediator.Send(new CancelOrderCommand(orderId,
+                                                             User.GetUserId(),
+                                                             false));
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
+
+    #region InitializePayment
+
+    [HttpPost(UserOrderEndpoints.Payment.InitializePayment)]
+    [SwaggerOperation(Summary = "ثبت پرداخت", Tags = new[] { "Order" })]
+    [SwaggerResponse(200, "success")]
+    [ProducesResponseType(typeof(Response<InitializePaymentResponseDto>), 200)]
+    public async Task<IActionResult> InitializePayment([FromQuery] string oId,
+            [FromQuery] decimal amount, [FromQuery] string callBack)
+    {
+        var payment = new InitializePaymentRequestDto(oId,
+                                                      amount,
+                                                      callBack,
+                                                      User.GetUserEmail());
+
+        var res = await Mediator.Send(new InitializePaymentRequestCommand(payment));
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
+
+    #region VerifyPayment
+
+    [HttpPost(UserOrderEndpoints.Payment.VerifyPayment)]
+    [SwaggerOperation(Summary = "تایید پرداخت", Tags = new[] { "Order" })]
+    [SwaggerResponse(200, "success")]
+    [SwaggerResponse(404, "not-found")]
+    [ProducesResponseType(typeof(Response<VerifyPaymentResponseDto>), 200)]
+    [ProducesResponseType(typeof(Response<string>), 404)]
+    public async Task<IActionResult> VerifyPayment([FromQuery] string authority, [FromQuery] string oId)
+    {
+        var res = await Mediator.Send(new VerifyPaymentRequestCommand(new VerifyPaymentRequestDto(oId, authority),
+                                                                      User.GetUserId()));
+
+        return JsonApiResult.Success(res);
+    }
+
+    #endregion
 }
--- src/WebApi/Shoppy.WebApi/Endpoints/User/UserOrderEndpoints.cs
+++ src/WebApi/Shoppy.WebApi/Endpoints/User/UserOrderEndpoints.cs
@@ -7,5 +7,18 @@ public class UserOrderEndpoints
     public static class Order
     {
         public const string GetOrders = Base + "/all";
+
+        public const string PlaceOrder = Base + "/place";
+
+        public const string CancelOrder = Base + "/cancel/{orderId}";
+    }
+
+    public static class Payment
+    {
+        private const string BasePayment = Base + "/payment";
+
+        public const string InitializePayment = BasePayment + "/init";
+
+        public const string VerifyPayment = BasePayment + "/verify";
     }
 }
