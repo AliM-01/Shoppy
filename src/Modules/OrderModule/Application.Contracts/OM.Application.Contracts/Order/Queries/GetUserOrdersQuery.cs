8c285643cdc982c1dd48f1112ac89a2809120b35
Ali M <alimousavi-01@outlook.com>
2022-04-07 12:55:21 +0430

    Created GetUserOrdersQuery

---
+++ src/Modules/OrderModule/Application.Contracts/OM.Application.Contracts/Order/Queries/GetUserOrdersQuery.cs
@@ -0,0 +1,5 @@
+﻿using OM.Application.Contracts.Order.DTOs;
+
+namespace OM.Application.Contracts.Order.Queries;
+
+public record GetUserOrdersQuery(string UserId) : IRequest<Response<List<OrderDto>>>;
