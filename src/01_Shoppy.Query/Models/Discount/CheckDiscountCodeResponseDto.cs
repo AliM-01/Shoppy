d080315e157ef0b41d8dd6d797e33129b8785fdd
Ali M <alimousavi-01@outlook.com>
2022-04-21 15:40:22 +0430

    Created CheckDiscountCodeResponseDto

---
+++ src/01_Shoppy.Query/Models/Discount/CheckDiscountCodeResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace _01_Shoppy.Query.Models.Discount;
+public class CheckDiscountCodeResponseDto
+{
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [JsonProperty("minutesUntillExpiration")]
+    public int MinutesUntillExpiration { get; set; }
+
+    [JsonProperty("isExpired")]
+    public bool IsExpired { get; set; }
+}
