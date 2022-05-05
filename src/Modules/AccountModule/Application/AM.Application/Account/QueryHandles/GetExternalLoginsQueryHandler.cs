c5f4629c870b63796070af01b1e00bec49bf6146
Ali M <alimousavi-01@outlook.com>
2022-05-05 14:51:52 +0430

    Implemented GetExternalLoginsQueryHandler

---
+++ src/Modules/AccountModule/Application/AM.Application/Account/QueryHandles/GetExternalLoginsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AM.Application.Contracts.Account.Queries;
+using Microsoft.AspNetCore.Authentication;
+
+namespace AM.Application.Account.QueryHandles;
+
+public class GetExternalLoginsQueryHandler : IRequestHandler<GetExternalLoginsQuery, ApiResult<IEnumerable<AuthenticationScheme>>>
+{
+    #region Ctor
+
+    private readonly SignInManager<Domain.Account.Account> _signInManager;
+
+    public GetExternalLoginsQueryHandler(SignInManager<Domain.Account.Account> signInManager)
+    {
+        _signInManager = Guard.Against.Null(signInManager, nameof(_signInManager));
+    }
+
+    #endregion Ctor
+
+    public async Task<ApiResult<IEnumerable<AuthenticationScheme>>> Handle(GetExternalLoginsQuery request, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var externalLogins = await _signInManager.GetExternalAuthenticationSchemesAsync();
+
+        return ApiResponse.Success<IEnumerable<AuthenticationScheme>>(externalLogins);
+    }
+}
