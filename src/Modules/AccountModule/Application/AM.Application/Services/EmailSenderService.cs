cee0cb301e5693c276adbb9a045df799e904e19a
Ali M <alimousavi-01@outlook.com>
2022-05-15 19:30:38 +0430

    Added Polly Retry to Send Email

--- src/Modules/AccountModule/Application/AM.Application/Services/EmailSenderService.cs
+++ src/Modules/AccountModule/Application/AM.Application/Services/EmailSenderService.cs
@@ -1,6 +1,8 @@
 ﻿using AM.Application.Contracts.Services;
 using AM.Infrastructure.Persistence.Settings;
 using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
 using System.Net;
 using System.Net.Mail;
 
@@ -12,11 +14,21 @@ public class EmailSenderService : IEmailSenderService
 
     private readonly EmailSettings _emailSettings;
     private readonly ILogger<EmailSenderService> _logger;
+    private const int MAX_RETRIES = 3;
+    private AsyncRetryPolicy _retryPolicy;
 
     public EmailSenderService(IOptions<EmailSettings> options, ILogger<EmailSenderService> logger)
     {
         _emailSettings = options.Value;
         _logger = logger;
+        _retryPolicy = Policy.Handle<Exception>()
+                                .WaitAndRetryAsync(
+                                           retryCount: MAX_RETRIES,
+                                           sleepDurationProvider: times => TimeSpan.FromMilliseconds(times * 250), // 250ms == 0.25s
+                                           onRetry: (exception, sleepDuration, attemptNumber, context) =>
+                                           {
+                                               logger.LogError("Email Sending Error. Retrying in {0}. {1}/{2}", sleepDuration, attemptNumber, MAX_RETRIES);
+                                           });
     }
 
     #endregion
@@ -27,7 +39,7 @@ public class EmailSenderService : IEmailSenderService
     // Secure SSL 465
     public async Task<bool> SendEmail(string email, string subject, string body)
     {
-        try
+        return await _retryPolicy.ExecuteAsync<bool>(async () =>
         {
             using (SmtpClient client = new(_emailSettings.Host, _emailSettings.Port))
             {
@@ -44,14 +56,8 @@ public class EmailSenderService : IEmailSenderService
                 message.Body = body;
 
                 await client.SendMailAsync(message);
+                return true;
             }
-            return true;
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex.Message);
-            _logger.LogError("InnerException is: {0}", ex.InnerException);
-            return false;
-        }
+        });
     }
 }
