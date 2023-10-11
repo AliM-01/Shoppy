b1e3c89ee4750fce81393c1bd7dbe0b04524515a
Ali M <ali.mo.dev@gmail.com>
2023-10-12 01:06:55 +0330

    AM.Domain & Application fix

--- src/Modules/AccountModule/Infrastructure/AM.Infrastructure.Persistence/Settings/AccountDbSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿using _0_Framework.Infrastructure;
-
-namespace AM.Infrastructure.Persistence.Settings;
-
-public class AccountDbSettings : BaseDbSettings
-{
-    public string UserTokenCollection { get; set; }
-}
